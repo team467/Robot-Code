c77fc2122558e5e66b659e740286b8a4326251a9
Andrew F <43021241+arfrie22@users.noreply.github.com>
2021-10-21 19:46:39 -0400

    Added the rest of the buttons on the custom controller

--- src/main/java/frc/robot/controllers/CustomController2020.java
+++ src/main/java/frc/robot/controllers/CustomController2020.java
@@ -2,6 +2,15 @@ package frc.robot.controllers;
 
 public class CustomController2020 {
     public enum Buttons {
+        INTAKE_ARM(1),
+        INTAKE_ROLLER_FORWARD(2),
+        INTAKE_ROLLER_BACKWARD(3),
+        INDEX_AUTO(4),
+        INDEX_ROLLER_FORWARD(5),
+        INDEX_ROLLER_BACKWARD(6),
+        SHOOTER_AUTO(7),
+        SHOOTER_FLYWHEEL(8),
+        SHOOTER_SHOOT(9),
         CLIMB_LOCK(10),
         CLIMB_UP(11),
         CLIMB_DOWN(12);
