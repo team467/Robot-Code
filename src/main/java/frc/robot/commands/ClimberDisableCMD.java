1dc53e2a9190c2a6261264283e190442439f44e5
Andrew F <43021241+arfrie22@users.noreply.github.com>
2021-11-09 19:23:12 -0500

    Moved climber enable/disable to initialize

--- src/main/java/frc/robot/commands/ClimberDisableCMD.java
+++ src/main/java/frc/robot/commands/ClimberDisableCMD.java
@@ -15,12 +15,12 @@ public class ClimberDisableCMD extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        climber.enable();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        climber.enable();
     }
 
     // Called once the command ends or is interrupted.
