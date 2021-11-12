50b48cd54c856ccf8157065eadda9264d43de666
Andrew F <43021241+arfrie22@users.noreply.github.com>
2021-11-11 20:37:26 -0500

    moved climber to using a SpeedControllerEncoder

--- src/main/java/frc/robot/subsystems/Climber2020.java
+++ src/main/java/frc/robot/subsystems/Climber2020.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotConstants;
+import frc.robot.drive.MotorType;
+import frc.robot.drive.SpeedControllerEncoder;
+import frc.robot.drive.SpeedControllerFactory;
 
 public class Climber2020 extends SubsystemBase {
-    private CANSparkMax climberMotor = new CANSparkMax(RobotConstants.get().climber2020MotorId(), MotorType.kBrushless);
+    private SpeedControllerEncoder climberMotor = SpeedControllerFactory.create(RobotConstants.get().climber2020MotorId(), MotorType.SPARK_MAX_BRUSHLESS);
     private boolean enabled = false;
 
     public Climber2020() {
@@ -30,13 +30,15 @@ public class Climber2020 extends SubsystemBase {
     }
 
     public void up() {
-        if (this.isEnabled())
+        if (this.isEnabled()) {
             climberMotor.set(RobotConstants.get().climber2020UpSpeed());
+        }
     }
 
     public void down() {
-        if (this.isEnabled())
+        if (this.isEnabled()) {
             climberMotor.set(-RobotConstants.get().climber2020DownSpeed());
+        }
     }
 
     public void stop() {
@@ -47,8 +49,8 @@ public class Climber2020 extends SubsystemBase {
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
 
-        builder.addDoubleProperty(".climber_position", () -> climberMotor.getEncoder().getPosition(), null);
-        builder.addDoubleProperty(".climber_speed", () -> climberMotor.getEncoder().getVelocity(), null);
+        builder.addDoubleProperty(".climber_position", () -> climberMotor.getPosition(), null);
+        builder.addDoubleProperty(".climber_speed", () -> climberMotor.getVelocity(), null);
     }
 }
 
