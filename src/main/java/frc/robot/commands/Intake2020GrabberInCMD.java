c7328d0f44e7dc120bc1c4d6d9eeb3a352a2543b
Shivam Goel <Shivam0922@gmail.com>
2021-11-16 20:29:26 -0500

    Configured Grabberin CMD

--- src/main/java/frc/robot/commands/Intake2020GrabberInCMD.java
+++ src/main/java/frc/robot/commands/Intake2020GrabberInCMD.java
@@ -29,7 +29,9 @@ public class Intake2020GrabberInCMD extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_subsystem.grabberIn();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
