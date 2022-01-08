2a6acaf86ef75be32f0600b3654b16e55381c96e
Andrew F <43021241+arfrie22@users.noreply.github.com>
2022-01-08 16:04:20 -0500

    re-removed speed controller encoder and factory

--- src/main/java/frc/robot/motors/SpeedControllerEncoder.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.motors;
-
-import edu.wpi.first.wpilibj.SpeedController;
-
-public interface SpeedControllerEncoder extends SpeedController {
-    public double getPosition();
-    public double getVelocity();
-    public double getCurrent();
-}
