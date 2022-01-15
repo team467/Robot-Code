b20d643f871299b3709254e4438f7f14c89642d8
Adam Nativ <admlvntv@gmail.com>
2022-01-14 20:37:26 -0500

    remove unneeded spaces

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -33,7 +33,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     
@@ -75,7 +74,6 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
