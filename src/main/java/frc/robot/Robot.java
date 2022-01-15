ff73e06790635192dfec6d2a3484646545ca0eab
Adam Nativ <admlvntv@gmail.com>
2022-01-14 19:37:24 -0500

    for shivam to continue the work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -92,7 +92,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    LOGGER.error("Hello World!");
+    LOGGER.debug("Hello World!");
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
