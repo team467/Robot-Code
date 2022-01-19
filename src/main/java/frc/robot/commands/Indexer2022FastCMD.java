96961874884b6e982691bda5af547e2af3bdd75e
Adam Nativ <admlvntv@gmail.com>
2022-01-19 18:46:51 -0500

    Create indexer2022 commands

---
+++ src/main/java/frc/robot/commands/Indexer2022FastCMD.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Indexer2022;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Indexer2022FastCMD extends CommandBase {
+  private final Indexer2022 indexer;
+
+  /**
+   * Creates a new Indexer2022FastCMD.
+   *
+   * @param indexer The subsystem used by this command.
+   */
+  public Indexer2022FastCMD(Indexer2022 indexer) {
+    this.indexer = indexer;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(indexer);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    indexer.indexerFast();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
