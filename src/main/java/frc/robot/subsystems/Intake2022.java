1efbdd6b84a16a0d94df383f750f17f63515b74b
Adam Nativ <admlvntv@gmail.com>
2022-01-28 17:04:17 -0500

    Small fixes
    add logging to llamaneckbackward
    remove logger from intake2022
    remove subsystems from robot.java

--- src/main/java/frc/robot/subsystems/Intake2022.java
+++ src/main/java/frc/robot/subsystems/Intake2022.java
@@ -3,15 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotConstants;
-import frc.robot.logging.RobotLogManager;
 import frc.robot.motors.MotorControllerEncoder;
 import frc.robot.motors.MotorControllerFactory;
 import frc.robot.motors.MotorType;
 
-import org.apache.logging.log4j.Logger;
-
 public class Intake2022 extends SubsystemBase {
-    private static final Logger LOGGER = RobotLogManager.getMainLogger(Intake2022.class.getName());
 
     private MotorControllerEncoder intakeMotor;
 
