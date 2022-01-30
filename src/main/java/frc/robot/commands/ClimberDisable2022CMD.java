2fb67dc43edfec85705478164a9c2e2ca970f289
Rachael Laikangbam <rlaik127@gmail.com>
2022-01-30 11:51:28 -0800

    climber

--- src/main/java/frc/robot/commands/ClimberDisable2022CMD.java
+++ src/main/java/frc/robot/commands/ClimberDisable2022CMD.java
@@ -7,35 +7,35 @@ import org.apache.logging.log4j.Logger;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ClimberUp2022CMD extends CommandBase {
+public class ClimberDisable2022CMD extends CommandBase{
     private final Climber2022 climber;
 
     private static final Logger LOGGER = RobotLogManager.getMainLogger(Climber2022.class.getName());
 
 
-    public ClimberUp2022CMD(Climber2022 climber) {
+    public ClimberDisable2022CMD(Climber2022 climber) {
         this.climber = climber;
 
         addRequirements(climber);
     }
+    
 
-
-    @Override
+    @Override 
     public void initialize() {
-        LOGGER.info("Climber going up");
+        LOGGER.info("Climber disabled");
     }
 
 
     @Override 
     public void execute() {
-        climber.up();
+        climber.disable();
     }
 
     @Override 
     public void end(boolean interrupted) {}
 
 
-    @Override
+    @Override 
     public boolean isFinished() {
         return false;
     }
--- src/main/java/frc/robot/subsystems/Climber2022.java
+++ src/main/java/frc/robot/subsystems/Climber2022.java
@@ -13,7 +13,11 @@ import org.apache.logging.log4j.Logger;
 
 // down, up, enable, stop
 public class Climber2022 extends SubsystemBase {
-    private MotorControllerEncoder climberMotor = MotorControllerFactory.create(RobotConstants.get().climber2020MotorID(), MotorType.//);
+
+    private static final Logger LOGGER = RobotLogManager.getMainLogger(Climber2022.class.getName());
+
+
+    //private MotorControllerEncoder climberMotor = MotorControllerFactory.create(RobotConstants.get().climber2022MotorID(), MotorType.);
     private boolean enabled = false;
 
     public Climber2022() {
@@ -24,10 +28,13 @@ public class Climber2022 extends SubsystemBase {
 
     public void enable() {
         enabled = true;
+        LOGGER.info("Climber enabled");
     }
 
     public void disable() {
         enabled = false;
+        stop();
+        LOGGER.info("Climber disabled");
     }
 
     public boolean isEnabled() {
@@ -35,15 +42,24 @@ public class Climber2022 extends SubsystemBase {
     }
 
     public void up() {
-        //
+        if (enabled) {
+            LOGGER.info("Climbing Up!");
+        } else {
+            LOGGER.info("Climb up does not work when disabled!");
+        }
     }
 
     public void down() {
-        //
+        if (enabled) {
+            LOGGER.info("Climbing down");
+        } else {
+            LOGGER.info("Climb up doesn't work when disabled");
+        }
     }
 
     public void stop () {
-        climberMotor.set(0);
+        //climberMotor.set(0);
+        LOGGER.info("Climber stopped");
     }
 }
 
