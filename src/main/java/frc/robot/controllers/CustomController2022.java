176ee4e215699ea152946c7ab9277e1482cc87e2
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-02-02 18:54:09 -0500

    Send rainbow on boot

--- src/main/java/frc/robot/controllers/CustomController2022.java
+++ src/main/java/frc/robot/controllers/CustomController2022.java
@@ -7,5 +7,6 @@ public class CustomController2022 extends CustomControllerBase {
 
     public CustomController2022(int port) {
         super(port);
+        sendCommand(new byte[]{0x06, 0x04, 0x03, 0x03});
     }
 }
