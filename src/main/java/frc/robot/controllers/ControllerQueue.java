1e997260e86c2923823026cd748ff74eac9f4fe5
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-02-05 13:50:51 -0500

    added ability to get controllers from queue

--- src/main/java/frc/robot/controllers/ControllerQueue.java
+++ src/main/java/frc/robot/controllers/ControllerQueue.java
@@ -22,4 +22,8 @@ public class ControllerQueue {
             controller.updateQueue();
         });
     }
+
+    public CustomControllerBase get(int index) {
+        return controllers.get(index);
+    }
 }
--- src/main/java/frc/robot/controllers/CustomController2022.java
+++ src/main/java/frc/robot/controllers/CustomController2022.java
@@ -7,10 +7,6 @@ public class CustomController2022 extends CustomControllerBase {
 
     public CustomController2022(int port) {
         super(port);
-
-        for (byte i = 0; i < 6; i++) {
-            addCommandToQueue(new byte[]{0x06, 0x01, i, 0x01, (byte) (i * 20), (byte) (i * 10), 0x05});
-        }
-        
+        addCommandToQueue(new byte[]{0x06, 0x04, 0x03, 0x03});
     }
 }
