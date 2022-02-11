278aa81c22bd2660b6ece09964dd16ce8820afb4
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-02-11 16:32:07 -0500

    Test for command queue

--- src/main/java/frc/robot/controllers/CustomController2022.java
+++ src/main/java/frc/robot/controllers/CustomController2022.java
@@ -7,6 +7,9 @@ public class CustomController2022 extends CustomControllerBase {
 
     public CustomController2022(int port) {
         super(port);
-        addCommandToQueue(new byte[]{0x06, 0x04, 0x03, 0x03});
+        // addCommandToQueue(new byte[]{0x06, 0x04, 0x03, 0x03});
+        for (int i = 0; i < 6; i++) {
+            addCommandToQueue(new byte[]{0x06, 0x01, (byte) i, 0x01, (byte) ((i + 1) * (255/7)), 20, 127});
+        }
     }
 }
