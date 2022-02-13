831b8884d36471180cbdf2b17489bb33a16af381
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-02-12 20:10:23 -0500

    More progress as I switch branches constantly

--- src/main/java/frc/robot/commands/TurnAngleCMD.java
+++ src/main/java/frc/robot/commands/TurnAngleCMD.java
@@ -1,9 +1,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Gyro;
 import frc.robot.utilities.MathUtils;
@@ -14,6 +18,8 @@ private Gyro gyro;
 private double angle;
 DifferentialDriveKinematics diffDriveKinematics;
 DifferentialDriveOdometry diffDriveOdometry;
+DifferentialDriveVoltageConstraint autDriveVoltageConstraint;
+TrajectoryConfig config;
 
 public TurnAngleCMD(Drivetrain drivetrain, Gyro gyro, double angle) {
     this.drivetrain = drivetrain;
@@ -31,11 +37,31 @@ public TurnAngleCMD(Drivetrain drivetrain, Gyro gyro, double angle) {
 public void initialize() {
     gyro.reset();
     diffDriveOdometry = new DifferentialDriveOdometry(gyro.getRotation2d());
+    drivetrain.resetLeftPosition();
+    drivetrain.resetRightPosition();
+
+    autDriveVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(RobotConstants.get().driveForwardRightFF().getkS(),
+                                       RobotConstants.get().driveForwardRightFF().getkV(),
+                                       RobotConstants.get().driveForwardRightFF().getkA()),
+            diffDriveKinematics,
+            10);
+    
+    config =
+        new TrajectoryConfig(RobotConstants.get().driveMaxVelocity(),
+                RobotConstants.get().driveMaxAcceleration())
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(diffDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autDriveVoltageConstraint);
+    
+            
 }
 
 @Override
 public void execute() {
-    diffDriveOdometry.update(gyro.getRotation2d(), drivetrain.getLeftPosition(), drivetrain.getRightPosition())
+    diffDriveOdometry.update(gyro.getRotation2d(), drivetrain.getLeftPosition(), drivetrain.getRightPosition());
     drivetrain.arcadeDrive(0, direction * MathUtil.clamp(Math.abs(error)/10, 0.1, 0.5));
 }
 
