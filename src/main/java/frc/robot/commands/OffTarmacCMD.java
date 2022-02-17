1f6acd3e542d7934c5f882731a33823b7d5f5acc
Adam Nativ <admlvntv@gmail.com>
2022-02-16 19:59:39 -0500

    Add javadoc comment to OffTarmacCMD

--- src/main/java/frc/robot/commands/OffTarmacCMD.java
+++ src/main/java/frc/robot/commands/OffTarmacCMD.java
@@ -1,10 +1,9 @@
-// Autonomous Move backwards off the tarmac until bumpers clear the area (2 points).
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 
+/** Autonomous Move backwards off the tarmac until bumpers clear the area (2 points). */
 public class OffTarmacCMD extends SequentialCommandGroup {
   int TARMAC_DISTANCE = 0;
 
