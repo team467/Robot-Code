91398fbc444a5efd1d4011ffe3ce1645832c7458
wuhoo123 <wzwu23@students.shrewsbury.k12.ma.us>
2022-02-26 14:34:03 -0500

    Renaming

--- src/main/java/frc/robot/commands/OffTarmacAutoCMD.java
+++ src/main/java/frc/robot/commands/OffTarmacAutoCMD.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 
 /** Autonomous Move backwards off the tarmac until bumpers clear the area (2 points). */
-public class OffTarmacCMD extends SequentialCommandGroup {
+public class OffTarmacAutoCMD extends SequentialCommandGroup {
   int TARMAC_DISTANCE = 0;
 
-  public OffTarmacCMD(Drivetrain drivetrain, Gyro gyro) {
+  public OffTarmacAutoCMD(Drivetrain drivetrain, Gyro gyro) {
 
     addCommands(new DriveDistanceCMD(drivetrain, gyro, TARMAC_DISTANCE));
   }
