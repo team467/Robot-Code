57cc81a18e58e64da7cb548ce1e7ad10c95e3f2c
Aradig26 <90940565+Aradig26@users.noreply.github.com>
2023-02-18 15:29:58 -0500

    Merge conficts

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -39,17 +39,6 @@ public class Robot extends LoggedRobot {
     // lidar = new LidarLitePWM(new DigitalInput(1));
     Logger logger = Logger.getInstance();
 
-    // Record metadata
-    logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
-    logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
-    logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-    logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
-    logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
-    switch (BuildConstants.DIRTY) {
-      case 0 -> logger.recordMetadata("GitDirty", "All changes committed");
-      case 1 -> logger.recordMetadata("GitDirty", "Uncomitted changes");
-      default -> logger.recordMetadata("GitDirty", "Unknown");
-    }
 
     // Set up data receivers & replay source
     switch (RobotConstants.get().mode()) {
