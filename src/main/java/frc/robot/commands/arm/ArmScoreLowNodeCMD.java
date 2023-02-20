90170da7f092e636c5b9d759ffd31553f6f950bb
Ellery Harrington <Lreh83@gmail.com>
2023-02-20 13:19:18 -0500

    Rename ArmScoreCMD to ArmPositionCMD and use subclasses

--- src/main/java/frc/robot/commands/arm/ArmScoreLowNodeCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmScoreLowNodeCMD.java
@@ -2,9 +2,9 @@ package frc.robot.commands.arm;
 
 import frc.robot.subsystems.arm.Arm;
 
-public class ArmScoreLowNodeCMD extends ArmScoreCMD {
+public class ArmScoreLowNodeCMD extends ArmPositionCMD {
 
   public ArmScoreLowNodeCMD(Arm arm) {
-    super(arm, 0.1, 0);
+    super(arm, 0.1, 0.05);
   }
 }
