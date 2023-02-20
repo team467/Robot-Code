430209495cbff3990055a3b2531cc48888f1ee75
Adam Nativ <admlvntv@gmail.com>
2023-02-20 16:07:45 -0500

    Modify balancing to predict future

--- src/main/java/frc/robot/commands/auto/Balancing.java
+++ src/main/java/frc/robot/commands/auto/Balancing.java
@@ -1,11 +1,16 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drive.Drive;
 
 public class Balancing extends CommandBase {
-  Drive drive;
+  private Drive drive;
+  private Timer timer = new Timer();
+  private double prevZ = 0.0;
+  private double deltaZ = 0.0;
+
 
   public Balancing(Drive drive) {
     this.drive = drive;
@@ -13,9 +18,21 @@ public class Balancing extends CommandBase {
   }
 
   @Override
+  public void initialize() {
+    prevZ = drive.getGravVec()[2];
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
   public void execute() {
     double[] gravVec = drive.getGravVec();
-    if (Math.hypot(gravVec[0], gravVec[1]) > 0.2) {
+    deltaZ = gravVec[2] - prevZ;
+    prevZ = gravVec[2];
+    double nextZ = gravVec[2] + deltaZ;
+//    if (Math.abs(gravVec[2]) < 0.8) {
+    if (Math.abs(nextZ) < 0.8) {
+      timer.reset();
       drive.runVelocity(new ChassisSpeeds(1.6 * gravVec[0], 1.6 * gravVec[1], 0.0));
     } else {
       drive.runVelocity(new ChassisSpeeds());
@@ -25,5 +42,10 @@ public class Balancing extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     drive.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(1.0) && Math.abs(drive.getGravVec()[2]) > 0.8;
   }
 }
