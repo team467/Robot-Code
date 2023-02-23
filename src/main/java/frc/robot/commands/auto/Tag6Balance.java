bbe379660ffee65ff3dd8538a0bfdf4b9c44a8df
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2023-02-22 19:46:19 -0500

    Added balancing from Tag 6 and 8.

---
+++ src/main/java/frc/robot/commands/auto/Tag6Balance.java
@@ -0,0 +1,37 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.lib.holonomictrajectory.Waypoint;
+import frc.robot.FieldConstants;
+import frc.robot.commands.drive.GoToTrajectory;
+import frc.robot.subsystems.drive.Drive;
+import java.util.List;
+
+public class Tag6Balance extends SequentialCommandGroup 
+{
+  public Tag6Balance(Drive drive) 
+  {
+    addCommands(
+        new GoToTrajectory(
+            drive,
+            List.of(
+                new Waypoint(FieldConstants.aprilTags.get(6).getTranslation().toTranslation2d()),
+                new Waypoint(
+                    new Translation2d(
+                        FieldConstants.Community.outerX + 1,
+                        FieldConstants.aprilTags.get(6).getTranslation().toTranslation2d().getY())))),              
+        new GoToTrajectory(
+            drive, 
+            List.of(
+                new Waypoint(
+                    new Translation2d(
+                        FieldConstants.Community.outerX + 1,
+                        FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d().getY())),
+                new Waypoint(
+                    new Translation2d(
+                        FieldConstants.Community.chargingStationOuterX,
+                        FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d().getY())))),
+        new Balancing(drive));
+  }
+}
