192cbf9cd849e3344fbb8659522e4a7a7818e5f6
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2023-02-22 20:55:08 -0500

    Added speeds to tag 6 and 8 balance files.

--- src/main/java/frc/robot/commands/auto/Tag8Balance.java
+++ src/main/java/frc/robot/commands/auto/Tag8Balance.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.constraint.MaxVelocityConstraint;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.lib.holonomictrajectory.Waypoint;
 import frc.robot.FieldConstants;
@@ -20,7 +21,10 @@ public class Tag8Balance extends SequentialCommandGroup
                 new Waypoint(
                     new Translation2d(
                         FieldConstants.Community.outerX + 1,
-                        FieldConstants.aprilTags.get(8).getTranslation().toTranslation2d().getY())))),              
+                        FieldConstants.aprilTags.get(8).getTranslation().toTranslation2d().getY()))),
+            0,
+            0,
+            List.of(new MaxVelocityConstraint(0.4))),              
         new GoToTrajectory(
             drive, 
             List.of(
@@ -31,7 +35,10 @@ public class Tag8Balance extends SequentialCommandGroup
                 new Waypoint(
                     new Translation2d(
                         FieldConstants.Community.chargingStationOuterX,
-                        FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d().getY())))),
+                        FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d().getY()))),
+            0,
+            0,
+            List.of(new MaxVelocityConstraint(0.4))),
         new Balancing(drive));
   }
 }
