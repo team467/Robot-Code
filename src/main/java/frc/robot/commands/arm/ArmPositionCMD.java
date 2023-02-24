d2131be8a679218b5d1a997110aa277164223da3
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2023-02-24 17:33:13 -0500

    added armPositionSupplier

--- src/main/java/frc/robot/commands/arm/ArmPositionCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmPositionCMD.java
@@ -1,21 +1,30 @@
 package frc.robot.commands.arm;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmPositionConstants.ArmPosition;
 
 public class ArmPositionCMD extends CommandBase {
   private final Arm arm;
-  private final ArmPosition armPosition;
+  private final Supplier<ArmPosition> armPositionSupplier;
 
   public ArmPositionCMD(Arm arm, ArmPosition armPosition) {
     this.arm = arm;
-    this.armPosition = armPosition;
+    this.armPositionSupplier = ()->armPosition;
+    addRequirements(arm);
+  }
+
+  public ArmPositionCMD(Arm arm, Supplier<ArmPosition> armPosition) {
+    this.arm = arm;
+    this.armPositionSupplier = armPosition;
     addRequirements(arm);
   }
 
   @Override
   public void initialize() {
+    ArmPosition armPosition = armPositionSupplier.get();
     arm.setTargetPositions(armPosition.extendSetpoint, armPosition.rotateSetpoint);
   }
 
