5eca92d4a10818c14402bc29eee9fc941386382e
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2023-02-24 21:54:18 -0500

    change arm CMD leds to be within ArmCMD

--- src/main/java/frc/robot/commands/arm/ArmManualDownCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmManualDownCMD.java
@@ -2,15 +2,22 @@ package frc.robot.commands.arm;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.intakerelease.IntakeRelease;
+import frc.robot.subsystems.intakerelease.IntakeRelease.Wants;
+import frc.robot.subsystems.led.Led2023;
+import frc.robot.subsystems.led.Led2023.COLORS_467;
 
 public class ArmManualDownCMD extends CommandBase {
-  private Arm arm;
+   private Arm arm;
+  private Led2023 ledStrip;
+  private IntakeRelease intakerelease;
 
-  public ArmManualDownCMD(Arm arm) {
+  public ArmManualDownCMD(Arm arm, IntakeRelease intakerelease, Led2023 ledStrip) {
     this.arm = arm;
-
-    addRequirements(arm);
-  }
+    this.ledStrip = ledStrip;
+    this.intakerelease = intakerelease;
+    addRequirements(arm, intakerelease, ledStrip);
+    }
 
   @Override
   public void initialize() {
@@ -18,7 +25,13 @@ public class ArmManualDownCMD extends CommandBase {
   }
 
   @Override
-  public void execute() {}
+  public void execute() {
+    if (intakerelease.getWants()==Wants.CUBE||intakerelease.haveCube()) {
+      ledStrip.setAlternateColorsDown(COLORS_467.Purple, COLORS_467.White, COLORS_467.Black.getColor());
+      } else {
+      ledStrip.setAlternateColorsDown(COLORS_467.Gold, COLORS_467.White, COLORS_467.Black.getColor());
+      }
+  }
 
   @Override
   public void end(boolean interrupted) {
--- src/main/java/frc/robot/commands/arm/ArmManualUpCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmManualUpCMD.java
@@ -2,14 +2,21 @@ package frc.robot.commands.arm;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.intakerelease.IntakeRelease;
+import frc.robot.subsystems.intakerelease.IntakeRelease.Wants;
+import frc.robot.subsystems.led.Led2023;
+import frc.robot.subsystems.led.Led2023.COLORS_467;
 
 public class ArmManualUpCMD extends CommandBase {
   private final Arm arm;
-
-  public ArmManualUpCMD(Arm arm) {
+  private IntakeRelease intakerelease;
+  private Led2023 ledStrip;
+  
+  public ArmManualUpCMD(Arm arm, IntakeRelease intakerelease, Led2023 ledStrip) {
     this.arm = arm;
-
-    addRequirements(arm);
+    this.ledStrip = ledStrip;
+    this.intakerelease = intakerelease;
+    addRequirements(arm, intakerelease, ledStrip);
   }
 
   @Override
@@ -18,7 +25,13 @@ public class ArmManualUpCMD extends CommandBase {
   }
 
   @Override
-  public void execute() {}
+  public void execute() {
+    if (intakerelease.getWants()==Wants.CUBE||intakerelease.haveCube()) {
+      ledStrip.setAlternateColorsUp(COLORS_467.Purple, COLORS_467.White, COLORS_467.Black.getColor());
+      } else {
+      ledStrip.setAlternateColorsUp(COLORS_467.Gold, COLORS_467.White, COLORS_467.Black.getColor());
+      }
+  }
 
   @Override
   public void end(boolean interrupted) {
