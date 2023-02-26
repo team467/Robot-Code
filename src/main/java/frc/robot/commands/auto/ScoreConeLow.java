ffa341fa09d98d2c0fc9a3acbe8087ecde217f7d
EQMoore <eqm.emoore@hotmail.com>
2023-02-26 15:56:30 -0600

    made it work with changes made in main

--- src/main/java/frc/robot/commands/auto/ScoreConeLow.java
+++ src/main/java/frc/robot/commands/auto/ScoreConeLow.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.lib.holonomictrajectory.Waypoint;
 import frc.robot.FieldConstants;
+import frc.robot.commands.arm.ArmCalibrateCMD;
 import frc.robot.commands.arm.ArmScoreLowNodeCMD;
 import frc.robot.commands.drive.GoToTrajectory;
 import frc.robot.subsystems.arm.Arm;
@@ -13,19 +14,17 @@ import java.util.List;
 public class ScoreConeLow extends SequentialCommandGroup {
   public ScoreConeLow(Drive drive, Arm arm) {
     addCommands(
+        new ArmCalibrateCMD(arm),
         new GoToTrajectory(
             drive,
             List.of(
-                new Waypoint(FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d()))),
-        new GoToTrajectory(
-            drive,
-            List.of(
+                new Waypoint(FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d()),
                 new Waypoint(
                     new Translation2d(
                         FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d().getX()
-                            + 0.5,
+                            + 0.3,
                         FieldConstants.aprilTags.get(7).getTranslation().toTranslation2d().getY()
-                            + 0.001)))),
+                            + FieldConstants.Grids.nodeSeparationY)))),
         new ArmScoreLowNodeCMD(arm));
   }
 }
