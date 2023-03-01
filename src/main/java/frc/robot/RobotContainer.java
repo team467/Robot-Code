efed5b215a417440093ccfeae090e8ad0c93a504
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2023-02-28 20:05:24 -0500

    Add score and score/move out.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -39,6 +39,8 @@ import frc.robot.commands.arm.ArmShelfCMD;
 import frc.robot.commands.arm.ArmStopCMD;
 import frc.robot.commands.auto.AlignToNode;
 import frc.robot.commands.auto.ScoreConeHigh;
+import frc.robot.commands.auto.ScoreCubeNoMove;
+import frc.robot.commands.auto.ScoreThenMoveOut8;
 import frc.robot.commands.auto.better.MidToCommunity;
 import frc.robot.commands.drive.DriveWithJoysticks;
 import frc.robot.commands.drive.GoToTrajectory;
@@ -212,6 +214,8 @@ public class RobotContainer {
     //        autoChooser.addOption("Drive Foward and Come Back", new DriveFowardComeBack(drive));
     //        autoChooser.addOption(
     //            "Score then Drive Foward, then ballance", new ScoreDriveFowardBallance(drive));
+    autoChooser.addOption("Driveless Score", new ScoreCubeNoMove(arm, intakeRelease, led2023));
+    autoChooser.addOption("Score Then Move", new ScoreThenMoveOut8(drive, arm, intakeRelease, led2023));
 
     autoChooser.addOption(
         "Drive Characterization",
