64efa31a4821fa87f9e6b9f794572acb838715c5
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2023-03-01 18:54:25 -0500

    soft reset

--- src/main/java/frc/robot/commands/arm/ArmFloorCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmFloorCMD.java
@@ -3,10 +3,12 @@ package frc.robot.commands.arm;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmPositionConstants;
 import frc.robot.subsystems.led.Led2023;
+import frc.robot.subsystems.led.Led2023.ColorScheme;
 
 public class ArmFloorCMD extends ArmPositionCMD {
 
   public ArmFloorCMD(Arm arm, Led2023 ledStrip) {
     super(arm, ArmPositionConstants.FLOOR, ledStrip);
+    ledStrip.setCmdColorScheme(ColorScheme.FLOOR);
   }
 }
