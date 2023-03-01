cb402e8462278f52a2eccac59242ad96d1a31d25
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2023-02-28 19:08:39 -0500

    revert back to CANSparkMax  motor

--- src/main/java/frc/robot/subsystems/intakerelease/IntakeReleaseIOPhysical.java
+++ src/main/java/frc/robot/subsystems/intakerelease/IntakeReleaseIOPhysical.java
@@ -1,49 +1,47 @@
 package frc.robot.subsystems.intakerelease;
 
-import com.ctre.phoenix.ErrorCode;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
-import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxLimitSwitch;
 import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.subsystems.intakerelease.IntakeRelease.Wants;
 
 public class IntakeReleaseIOPhysical implements IntakeReleaseIO {
-  private final TalonSRX motor;
+  private final CANSparkMax motor;
+  private final RelativeEncoder encoder;
   private final DigitalInput cubeLimitSwitch;
-  private final ErrorCode coneLimitSwitch;
+  private final SparkMaxLimitSwitch coneLimitSwitch;
 
   public IntakeReleaseIOPhysical(int motorID, int cubeLimID) {
-    motor = new TalonSRX(motorID);
-    motor.setNeutralMode(NeutralMode.Brake);
+    motor = new CANSparkMax(motorID, MotorType.kBrushless);
+    encoder = motor.getEncoder();
+    motor.setIdleMode(IdleMode.kBrake);
     motor.setInverted(false);
-    motor.enableVoltageCompensation(true);
-    motor.configVoltageCompSaturation(12, 0);
+    motor.enableVoltageCompensation(12);
     cubeLimitSwitch = new DigitalInput(cubeLimID);
-    coneLimitSwitch =
-        motor.configReverseLimitSwitchSource(
-            LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
+    coneLimitSwitch = motor.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
   }
 
   @Override
   public void setPercent(double percent) {
-    motor.set(ControlMode.PercentOutput, percent);
+    motor.set(percent);
   }
 
   @Override
   public void setVoltage(double volts) {
-    motor.set(ControlMode.PercentOutput, volts / 12);
+    motor.setVoltage(volts);
   }
 
   @Override
   public void updateInputs(IntakeReleaseIOInputs inputs, Wants wants) {
-    inputs.motorPosition = motor.getSelectedSensorPosition();
-    inputs.motorVelocity = motor.getSelectedSensorVelocity();
-    inputs.motorCurrent = motor.getStatorCurrent();
-    inputs.motorAppliedVolts = motor.getBusVoltage() * motor.getMotorOutputVoltage();
+    inputs.motorPosition = encoder.getPosition();
+    inputs.motorVelocity = encoder.getVelocity();
+    inputs.motorAppliedVolts = motor.getBusVoltage() * motor.getAppliedOutput();
+    inputs.motorCurrent = motor.getOutputCurrent();
     inputs.cubeLimitSwitch = !cubeLimitSwitch.get();
-    inputs.coneLimitSwitch = motor.isRevLimitSwitchClosed() == 1;
+    inputs.coneLimitSwitch = coneLimitSwitch.isPressed();
     inputs.wantsCone = wants == Wants.CONE;
     inputs.wantsCube = wants == Wants.CUBE;
   }
