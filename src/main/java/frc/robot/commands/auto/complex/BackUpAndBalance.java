56f490f95a557bbcd37b00e84e2c872defe10351
bryanduerk <bryan.duerk@gmail.com>
2023-03-09 15:13:12 -0500

    Full complex move set

--- src/main/java/frc/robot/commands/auto/complex/BackUpAndBalance.java
+++ src/main/java/frc/robot/commands/auto/complex/BackUpAndBalance.java
@@ -1,35 +1,24 @@
 package frc.robot.commands.auto.complex;
 
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.arm.ArmHomeCMD;
 import frc.robot.commands.auto.Balancing;
 import frc.robot.commands.auto.Initialize;
-import frc.robot.commands.auto.Score;
 import frc.robot.commands.auto.StraightDriveToPose;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.intakerelease.IntakeRelease;
 import frc.robot.subsystems.led.Led2023;
 
-public class ScoreAndBackUpAndBalance extends SequentialCommandGroup {
-  public ScoreAndBackUpAndBalance(
+public class BackUpAndBalance extends SequentialCommandGroup {
+  public BackUpAndBalance(
       String relativePosition,
-      String gamePieceType,
-      String location,
       Drive drive,
       Arm arm,
-      IntakeRelease intakeRelease,
       Led2023 ledStrip) {
     int aprilTag = 7;
     addCommands(
         new Initialize(aprilTag, relativePosition, drive, arm, ledStrip),
-        new Score(gamePieceType, location, arm, intakeRelease, ledStrip),
-        Commands.parallel(
-            new StraightDriveToPose(Units.inchesToMeters(170.0), 0.0, 0.0, drive),
-            new ArmHomeCMD(arm, ledStrip)
-        ),
+        new StraightDriveToPose(Units.inchesToMeters(170.0), 0.0, 0.0, drive),
         new StraightDriveToPose(Units.inchesToMeters(-75.0), 0.0, 0.0, drive),
         new Balancing(drive));
   }
--- src/main/java/frc/robot/commands/auto/complex/OnlyBalance.java
+++ src/main/java/frc/robot/commands/auto/complex/OnlyBalance.java
@@ -1,36 +1,26 @@
 package frc.robot.commands.auto.complex;
 
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.arm.ArmHomeCMD;
 import frc.robot.commands.auto.Balancing;
 import frc.robot.commands.auto.Initialize;
-import frc.robot.commands.auto.Score;
 import frc.robot.commands.auto.StraightDriveToPose;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.intakerelease.IntakeRelease;
 import frc.robot.subsystems.led.Led2023;
 
-public class ScoreAndBalance extends SequentialCommandGroup {
-
-  public ScoreAndBalance(
+public class OnlyBalance extends SequentialCommandGroup {
+  
+  public OnlyBalance(
       String relativePosition,
-      String gamePieceType,
-      String location,
       Drive drive,
       Arm arm,
-      IntakeRelease intakeRelease,
       Led2023 ledStrip) {
     int aprilTag = 7;
     addCommands(
         new Initialize(aprilTag, relativePosition, drive, arm, ledStrip),
-        new Score(gamePieceType, location, arm, intakeRelease, ledStrip),
-        Commands.parallel(
-          new StraightDriveToPose(Units.inchesToMeters(95.25), 0.0, 0.0, drive),
-          new ArmHomeCMD(arm, ledStrip)
-        ),
+        new StraightDriveToPose(Units.inchesToMeters(95.25), 0.0, 0.0, drive),
         new Balancing(drive));
   }
+  
 }
