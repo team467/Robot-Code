0264ff547a7bee57022a4838b3d52adfe7fcb56a
Ellery Harrington <Lreh83@gmail.com>
2023-03-25 17:55:38 -0400

    Tweak arm positions

--- src/main/java/frc/robot/subsystems/arm/ArmPositionConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmPositionConstants.java
@@ -11,11 +11,11 @@ public class ArmPositionConstants {
     }
   }
 
-  public static final ArmPosition MID_CONE = new ArmPosition(0.303, 0.167);
+  public static final ArmPosition MID_CONE = new ArmPosition(0.303, 0.156);
   public static final ArmPosition MID_CUBE = new ArmPosition(0.144, 0.114);
   public static final ArmPosition HIGH_CONE = new ArmPosition(0.610, 0.187);
   public static final ArmPosition HIGH_CUBE = new ArmPosition(0.444, 0.154);
-  public static final ArmPosition LOW_BOTH = new ArmPosition(0.137, 0.037);
+  public static final ArmPosition LOW_BOTH = new ArmPosition(0.088, 0.035);
   public static final ArmPosition HOME = new ArmPosition(0.02, 0.0);
   public static final ArmPosition FLOOR = new ArmPosition(0.185, 0.025);
   public static final ArmPosition SHELF_CONE = new ArmPosition(0.086, 0.159);
