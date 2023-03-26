b42296e1aecca0f2334ca579675c75504dcc156a
Ellery Harrington <Lreh83@gmail.com>
2023-03-26 12:17:51 -0400

    ProfiledPIDController for arm extend

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -476,7 +478,8 @@ public class Arm extends SubsystemBase {
   }
 
   private class Pids {
-    private PIDController extendPidController = new PIDController(50, 0, 0);
+    private ProfiledPIDController extendPidController =
+        new ProfiledPIDController(50, 0, 0, new TrapezoidProfile.Constraints(10, 10));
     private PIDController rotatePidController = new PIDController(600, 0, 0);
     private boolean calculatedOnPeriodic = false;
 
@@ -503,7 +506,7 @@ public class Arm extends SubsystemBase {
     public Pids setExtendSetpoint(double target) {
       if (this.extendSetpoint != target) {
         this.extendSetpoint = target;
-        extendPidController.setSetpoint(target);
+        extendPidController.setGoal(target);
       }
       return this;
     }
