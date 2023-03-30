86e511d9d45b49a89167134924d76e723ddc9843
Adam Nativ <admlvntv@gmail.com>
2023-03-29 20:59:09 -0400

    More faster for cones

--- src/main/java/frc/robot/commands/intakerelease/ReleaseCMD.java
+++ src/main/java/frc/robot/commands/intakerelease/ReleaseCMD.java
@@ -3,13 +3,25 @@ package frc.robot.commands.intakerelease;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.ArmDropCMD;
+import frc.robot.commands.arm.ArmHomeCMD;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.intakerelease.IntakeRelease;
 
 public class ReleaseCMD extends SequentialCommandGroup {
   public ReleaseCMD(IntakeRelease intakerelease, Arm arm) {
-    addCommands(
-        new ArmDropCMD(intakerelease::haveCone, intakerelease::wantsCone, arm),
-        Commands.run(intakerelease::release, intakerelease).withTimeout(0.5));
+    if (intakerelease.wantsCone()) {
+      addCommands(
+          new ArmDropCMD(intakerelease::haveCone, intakerelease::wantsCone, arm).withTimeout(0.3),
+          Commands.parallel(
+              Commands.run(intakerelease::release, intakerelease).withTimeout(0.5),
+              new ArmHomeCMD(arm)).withTimeout(5.0)
+      );
+    } else {
+      addCommands(
+          new ArmDropCMD(intakerelease::haveCone, intakerelease::wantsCone, arm).withTimeout(0.3),
+          Commands.run(intakerelease::release, intakerelease).withTimeout(0.5),
+          new ArmHomeCMD(arm).withTimeout(5.0)
+      );
+    }
   }
 }
