971f450c7ffdcfe3d686c59e2e3cf125414e2b2d
Aradig26 <90940565+Aradig26@users.noreply.github.com>
2023-03-31 19:40:18 -0400

    fix and format

--- src/main/java/frc/robot/commands/arm/ArmFloorCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmFloorCMD.java
@@ -1,7 +1,9 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.intakerelease.IntakeAndRaise;
+import frc.robot.commands.intakerelease.IntakeCMD;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmPositionConstants;
 import frc.robot.subsystems.intakerelease.IntakeRelease;
@@ -10,12 +12,14 @@ public class ArmFloorCMD extends SequentialCommandGroup {
 
   public ArmFloorCMD(Arm arm, IntakeRelease intakeRelease) {
     addCommands(
-        new ArmPositionCMD(
-            arm,
-            () ->
-                intakeRelease.wantsCone()
-                    ? ArmPositionConstants.CONE_FLOOR
-                    : ArmPositionConstants.CUBE_FLOOR),
+        Commands.parallel(
+            new ArmPositionCMD(
+                arm,
+                () ->
+                    intakeRelease.wantsCone()
+                        ? ArmPositionConstants.CONE_FLOOR
+                        : ArmPositionConstants.CUBE_FLOOR),
+            new IntakeCMD(intakeRelease)),
         new IntakeAndRaise(arm, intakeRelease));
   }
 }
--- src/main/java/frc/robot/commands/arm/ArmShelfCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmShelfCMD.java
@@ -1,7 +1,9 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.intakerelease.IntakeAndRaise;
+import frc.robot.commands.intakerelease.IntakeCMD;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmPositionConstants;
 import frc.robot.subsystems.intakerelease.IntakeRelease;
@@ -10,12 +12,14 @@ public class ArmShelfCMD extends SequentialCommandGroup {
 
   public ArmShelfCMD(Arm arm, IntakeRelease intakeRelease) {
     addCommands(
-        new ArmPositionCMD(
-            arm,
-            () ->
-                intakeRelease.wantsCone()
-                    ? ArmPositionConstants.SHELF_CONE
-                    : ArmPositionConstants.SHELF_CUBE),
+        Commands.parallel(
+            new ArmPositionCMD(
+                arm,
+                () ->
+                    intakeRelease.wantsCone()
+                        ? ArmPositionConstants.SHELF_CONE
+                        : ArmPositionConstants.SHELF_CUBE),
+            new IntakeCMD(intakeRelease)),
         new IntakeAndRaise(arm, intakeRelease));
   }
 }
