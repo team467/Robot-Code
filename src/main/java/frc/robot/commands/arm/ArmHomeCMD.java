354de8f578578f435d1239cdfe407c61fa1d3755
Adam Nativ (At Event) <admlvntv@gmail.com>
2023-04-06 10:55:15 -0400

    Fix arm rotation tolerance

--- src/main/java/frc/robot/commands/arm/ArmHomeCMD.java
+++ src/main/java/frc/robot/commands/arm/ArmHomeCMD.java
@@ -1,11 +1,12 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmPositionConstants;
 
-public class ArmHomeCMD extends ArmPositionCMD {
+public class ArmHomeCMD extends SequentialCommandGroup {
 
   public ArmHomeCMD(Arm arm) {
-    super(arm, ArmPositionConstants.HOME);
+    addCommands(new ArmPositionCMD(arm, ArmPositionConstants.HOME).withTimeout(5.0));
   }
 }
