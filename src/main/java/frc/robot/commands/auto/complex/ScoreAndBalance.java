6fdeb7ddd680964dd2f163d0d96d10d23eb38c35
Adam Nativ (At Event) <admlvntv@gmail.com>
2023-04-08 08:45:48 -0400

    Fix auto balance

--- src/main/java/frc/robot/commands/auto/complex/ScoreAndBalance.java
+++ src/main/java/frc/robot/commands/auto/complex/ScoreAndBalance.java
@@ -3,6 +3,7 @@ package frc.robot.commands.auto.complex;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.FieldConstants;
 import frc.robot.commands.arm.ArmHomeCMD;
 import frc.robot.commands.auto.BetterBalancing;
 import frc.robot.commands.auto.Initialize;
@@ -27,7 +28,15 @@ public class ScoreAndBalance extends SequentialCommandGroup {
         new Score(gamePieceType, location, arm, intakeRelease),
         Commands.sequence(
             new ArmHomeCMD(arm, intakeRelease::wantsCone).withTimeout(3.5),
-            new StraightDriveToPose(Units.inchesToMeters(95.25), 0.0, 0.0, drive).withTimeout(2.5)),
+            Commands.sequence(
+                new StraightDriveToPose(
+                        0.0,
+                        -FieldConstants.Grids.nodeSeparationY - Units.inchesToMeters(1.0),
+                        0.0,
+                        drive)
+                    .withTimeout(1.0),
+                new StraightDriveToPose(Units.inchesToMeters(95.25), 0.0, 0.0, drive)
+                    .withTimeout(2.5))),
         new BetterBalancing(drive));
   }
 }
