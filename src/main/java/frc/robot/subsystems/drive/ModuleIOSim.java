b98f20c511aed14f8ff3c273093cb6a1a3438f37
Adam Nativ <admlvntv@gmail.com>
2023-12-05 16:57:35 -0500

    MotorSims can be final

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
@@ -10,8 +10,8 @@ public class ModuleIOSim implements ModuleIO {
   private static final double LOOP_PERIOD_SECS = 0.02;
   private final double RADS_TO_METERS = RobotConstants.get().moduleWheelDiameter() / 2;
 
-  private DCMotorSim driveSim = new DCMotorSim(DCMotor.getNEO(1), 6.75, 0.025);
-  private DCMotorSim turnSim = new DCMotorSim(DCMotor.getNEO(1), 150.0 / 7.0, 0.004);
+  private final DCMotorSim driveSim = new DCMotorSim(DCMotor.getNEO(1), 6.75, 0.025);
+  private final DCMotorSim turnSim = new DCMotorSim(DCMotor.getNEO(1), 150.0 / 7.0, 0.004);
 
   private final Rotation2d turnAbsoluteInitPosition = new Rotation2d(Math.random() * 2.0 * Math.PI);
   private double driveAppliedVolts = 0.0;
