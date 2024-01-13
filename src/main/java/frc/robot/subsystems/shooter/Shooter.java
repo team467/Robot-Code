e5e27c1dff8280364a87068a38becbdbd7190492
EQMoore <eqm.emoore@hotmail.com>
2024-01-13 15:24:01 -0600

    Continues basic shooter implimentation

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -7,22 +7,57 @@ package frc.robot.subsystems.shooter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-  private final ShooterConstants constants;
+  enum IndexerState {
+    FOWARD,
+    HOLD,
+    BACKWARD
+  }
+
+  IndexerState indxState = IndexerState.HOLD;
+
   /** Creates a new shooter. */
   private final ShooterIO io;
 
   private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
 
-  public Shooter(ShooterIO io, ShooterConstants constants) {
+  public Shooter(ShooterIO io) {
     this.io = io;
-    this.constants = constants;
   }
 
   public void periodic() {
     io.updateInputs(inputs);
+    switch (indxState) {
+      case FOWARD:
+        io.setIndexerVoltage(ShooterConstants.indexerFowardVoltage);
+        break;
+      case HOLD:
+        io.setIndexerVoltage(ShooterConstants.indexerHoldVoltage);
+        break;
+      case BACKWARD:
+        io.setIndexerVoltage(ShooterConstants.indexerBackwardVolatage);
+        break;
+      default:
+        break;
+    }
   }
 
-  public void setIndexerForward() {
-    io.setIndexerVoltage(constants.indexerFowardVoltage);
+  public void setIndexeVoltage(double volts) {
+    io.setIndexerVoltage(volts);
   }
+
+  public void SetIndexerState(IndexerState indxState) {
+    this.indxState = indxState;
+}
+
+public void setShooterVoltage(double volts) {
+  io.setShooterVoltage(volts);
+}
+
+public boolean getShooterIsReady() {
+  return inputs.shooterVelocityRadPerSec >= ShooterConstants.shooterReadyVelocity;
+}
+
+public boolean getHoldingNote() {
+  return true;
+}
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -4,17 +4,30 @@ import frc.robot.Constants;
 
 public class ShooterConstants {
   public static double indexerFowardVoltage;
+  public static double indexerHoldVoltage;
+  public static double indexerBackwardVolatage;
+  public static double shooterReadyVelocity;
 
   static {
     switch (Constants.getRobot()) {
       case ROBOT_2023 -> {
         indexerFowardVoltage = 5.0;
+        indexerHoldVoltage = 0.0;
+        indexerBackwardVolatage = -3.0;
+        shooterReadyVelocity = 0.6;
       }
       case ROBOT_SIMBOT -> {
         indexerFowardVoltage = 0.0;
+        indexerHoldVoltage = 0.0;
+        indexerBackwardVolatage = 0.0;
+        shooterReadyVelocity = 0.0;
+        
       }
       default -> {
         indexerFowardVoltage = 0.0;
+        indexerHoldVoltage = 0.0;
+        indexerBackwardVolatage = 0.0;
+        shooterReadyVelocity = 0.0;
       }
     }
   }
