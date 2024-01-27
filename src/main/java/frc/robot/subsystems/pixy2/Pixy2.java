ded4fc5fa5ccb3daab34bf466361eaec090e8f0b
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-01-27 16:40:07 -0500

    format

--- src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
+++ src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
@@ -14,25 +14,32 @@ public class Pixy2 extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
   }
+
   public double getAge() {
     return inputs.age;
   }
+
   public double getX() {
-      return inputs.x;
+    return inputs.x;
   }
+
   public double getY() {
-      return inputs.y;
+    return inputs.y;
   }
+
   public double getAngle() {
-      return inputs.angle;
+    return inputs.angle;
   }
+
   public double getSignature() {
-      return inputs.signature;
+    return inputs.signature;
   }
+
   public double getWidth() {
-      return inputs.width;
+    return inputs.width;
   }
+
   public double getHeight() {
-      return inputs.height;
+    return inputs.height;
   }
 }
