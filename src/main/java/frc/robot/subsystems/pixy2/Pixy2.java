a95303972604eb1bb5e5fee251f1df7bead13049
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-01-27 16:28:10 -0500

    create methods to get inputs

--- src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
+++ src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
@@ -14,4 +14,25 @@ public class Pixy2 extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
   }
+  public double getAge() {
+    return inputs.age;
+  }
+  public double getX() {
+      return inputs.x;
+  }
+  public double getY() {
+      return inputs.y;
+  }
+  public double getAngle() {
+      return inputs.angle;
+  }
+  public double getSignature() {
+      return inputs.signature;
+  }
+  public double getWidth() {
+      return inputs.width;
+  }
+  public double getHeight() {
+      return inputs.height;
+  }
 }
