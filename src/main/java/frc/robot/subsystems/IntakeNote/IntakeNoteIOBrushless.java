432139c77dc5ffb477b4dd8b8f26149f8755aa09
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2024-01-27 16:44:49 -0500

    Added updateInputs() to motor controller file for logging of appliedVolts and motorVelocity.

--- src/main/java/frc/robot/subsystems/IntakeNote/IntakeNoteIOBrushless.java
+++ src/main/java/frc/robot/subsystems/IntakeNote/IntakeNoteIOBrushless.java
@@ -3,26 +3,28 @@ package frc.robot.subsystems.IntakeNote;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 public class IntakeNoteIOBrushless implements IntakeNoteIO {
   private final CANSparkMax motor;
+  private final RelativeEncoder intakEncoder;
+
+  //private final RelativeEncoder intakeEncoder;
 
   public IntakeNoteIOBrushless(int motorID) {
     motor = new CANSparkMax(motorID, MotorType.kBrushless);
     motor.setIdleMode(IdleMode.kBrake);
     motor.setInverted(false);
     motor.enableVoltageCompensation(12);
+    intakEncoder = motor.getEncoder();
   }
 
   public void setSpeed(double speed) {
     motor.set(speed);
   }
 
-  public double getSpeed() {
-    return motor.get();
-  }
-
-  public double getAppliedVolts() {
-    return motor.getBusVoltage();
+  public void updateInputs(IntakeNoteIOInputs inputs) {
+    inputs.appliedVolts = motor.getBusVoltage() * motor.getAppliedOutput();
+    inputs.motorVelocity = intakEncoder.getVelocity();
   }
 }
