ad8041bf2cf1fdb971a2640a1a0fdefdfb046513
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-01-29 20:39:52 -0500

    filter angle

--- src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
+++ src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems.pixy2;
 
+import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
 public class Pixy2 extends SubsystemBase {
   private final Pixy2IO io;
   private final Pixy2IOInputsAutoLogged inputs = new Pixy2IOInputsAutoLogged();
+
+  private final MedianFilter angleFilter = new MedianFilter(5);
+  private double filteredAngle;
 
   public Pixy2(Pixy2IO io) {
     this.io = io;
@@ -16,6 +20,8 @@ public class Pixy2 extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Pixy2", inputs);
+
+    filteredAngle = angleFilter.calculate(inputs.angle);
   }
 
   public double getAge() {
@@ -31,7 +37,7 @@ public class Pixy2 extends SubsystemBase {
   }
 
   public double getAngle() {
-    return inputs.angle;
+    return filteredAngle;
   }
 
   public double getSignature() {
