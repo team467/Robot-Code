bd35ef775c27836d31fb1a517869a7e6e4c17663
Adam Nativ <admlvntv@gmail.com>
2024-02-03 13:03:25 -0500

    Tuning mode should be false

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
   private static final RobotType ROBOT_TYPE_OVERRIDE = null;
 
   public static final Measure<Time> loopPeriod = Milliseconds.of(20);
-  public static final boolean tuningMode = true;
+  public static final boolean tuningMode = false;
 
   private static final String ROBOT_FILENAME = "/home/lvuser/robot";
   private static RobotType cachedRobotTypeFromRoborio = null;
