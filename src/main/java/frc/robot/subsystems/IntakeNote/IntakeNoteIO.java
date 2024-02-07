31aa8d8c122f42aaaa6a0999d5da12984e6e0aea
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2024-02-07 16:07:52 -0500

    Removed un-needed variables from IO class.

--- src/main/java/frc/robot/subsystems/IntakeNote/IntakeNoteIO.java
+++ src/main/java/frc/robot/subsystems/IntakeNote/IntakeNoteIO.java
@@ -5,12 +5,6 @@ import org.littletonrobotics.junction.AutoLog;
 public interface IntakeNoteIO {
   @AutoLog
   class IntakeNoteIOInputs {
-    // I/p for intake
-    public boolean seesNote = false; // Need to communicate with Vision
-    public boolean armSwitchClosed = false;
-
-    // O/p for intake
-
     // Motor IO
     public double positionRads = 0.0;
     public double velocityRadsPerSec = 0.0;
