7a5188858e1f3486d323f799a2d95642e4e93b09
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-02-10 17:42:51 -0500

    rewrite 2Note Auto

--- src/main/java/frc/robot/commands/auto/Autos.java
+++ src/main/java/frc/robot/commands/auto/Autos.java
@@ -7,14 +7,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.utils.AllianceFlipUtil;
 import frc.robot.FieldConstants;
-import frc.robot.commands.drive.DriveWithDpad;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterConstants;
 import java.util.Set;
+import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 public class Autos {
@@ -38,95 +39,61 @@ public class Autos {
     RIGHT
   }
 
-  public Command oneNoteAuto() {
+  private Supplier<Pose2d> getSpeakerTargetPose() {
     Translation2d speaker =
         AllianceFlipUtil.apply(FieldConstants.Speaker.centerSpeakerOpening.toTranslation2d());
-    Translation2d currentTranslation = drive.getPose().getTranslation();
-    Pose2d targetPose =
-        new Pose2d(currentTranslation, speaker.minus(currentTranslation).getAngle());
-    Logger.recordOutput("Autos/oneNoteAuto/targetPose", targetPose);
-    Logger.recordOutput("Autos/oneNoteAuto/currentTranslation", currentTranslation);
-    Logger.recordOutput("Autos/oneNoteAuto/speaker", speaker);
-    return Commands.defer(() -> new StraightDriveToPose(targetPose, drive), Set.of(drive))
-        //.andThen(new DriveWithDpad(drive, () -> 0))
-    .andThen(shooter.shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC));
+    Supplier<Pose2d> targetPose =
+        () ->
+            new Pose2d(
+                drive.getPose().getTranslation(),
+                speaker.minus(drive.getPose().getTranslation()).getAngle());
+    Logger.recordOutput("Autos/getSpeakerTargetPose/targetPose", targetPose.get());
+    Logger.recordOutput("Autos/getSpeakerTargetPose/speaker", speaker);
+    return targetPose;
+  }
+
+  public Command oneNoteAuto() {
+    return Commands.defer(
+            () -> new StraightDriveToPose(getSpeakerTargetPose().get(), drive), Set.of(drive))
+        .andThen(shooter.shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC));
   }
 
   public Command twoNoteAuto(StartingPosition position) {
-
-    return switch (position) {
-      case LEFT -> shooter
-          .shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC)
-          .andThen(
-              Commands.parallel(
-                  new StraightDriveToPose(
-                      new Pose2d(
-                          // TODO: Add the correct pose for the two note auto
-                          ),
-                      drive),
-                  arm.toSetpoint(
-                      new Rotation2d(-9000) // TODO: Add the correct setpoint for the two note auto
-                      )))
-          .andThen(Commands.parallel(intake.intake(), indexer.setIndexerPercentVelocity(0.25)))
-          .andThen(
-              new StraightDriveToPose(
-                  new Pose2d(
-                      // TODO: Add the correct pose for the two note auto
-                      ),
-                  drive))
-          .andThen(
-              arm.toSetpoint(
-                  new Rotation2d(-9000)
-                  // TODO: Add the correct setpoint for the two note auto
-                  ))
-          .andThen(shooter.shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC));
-      case CENTER ->
-      //              shooter
-      //          .shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC)
-      (Commands.parallel(
-              new StraightDriveToPose(new Pose2d(1.0, 1.0, new Rotation2d()), drive),
-              arm.toSetpoint(
-                  new Rotation2d(-9000) // TODO: Add the correct setpoint for the two note auto
-                  )))
-          .andThen(Commands.parallel(intake.intake(), indexer.setIndexerPercentVelocity(0.25)))
-          .andThen(
-              new StraightDriveToPose(
-                  new Pose2d(
-                      // TODO: Add the correct pose for the two note auto
-                      ),
-                  drive))
-          .andThen(
-              arm.toSetpoint(
-                  new Rotation2d(-9000)
-                  // TODO: Add the correct setpoint for the two note auto
-                  ))
-          .andThen(shooter.shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC));
-      case RIGHT -> shooter
-          .shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC)
-          .andThen(
-              Commands.parallel(
-                  new StraightDriveToPose(
-                      new Pose2d(
-                          // TODO: Add the correct pose for the two note auto
-                          ),
-                      drive),
-                  arm.toSetpoint(
-                      new Rotation2d(-9000) // TODO: Add the correct setpoint for the two note auto
-                      )))
-          .andThen(Commands.parallel(intake.intake(), indexer.setIndexerPercentVelocity(0.25)))
-          .andThen(
-              new StraightDriveToPose(
-                  new Pose2d(
-                      // TODO: Add the correct pose for the two note auto
-                      ),
-                  drive))
-          .andThen(
-              arm.toSetpoint(
-                  new Rotation2d(-9000)
-                  // TODO: Add the correct setpoint for the two note auto
-                  ))
-          .andThen(shooter.shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC));
-      default -> Commands.none();
-    };
+    Pose2d notePose;
+    switch (position) {
+      case LEFT -> notePose = new Pose2d(); // TODO: Add the correct pose for the two note auto
+      case CENTER -> notePose = new Pose2d();
+      case RIGHT -> notePose = new Pose2d();
+      default -> notePose = new Pose2d();
+    }
+    ;
+    return Commands.defer(
+            () -> new StraightDriveToPose(getSpeakerTargetPose().get(), drive), Set.of(drive))
+        .andThen(
+            shooter
+                .shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC)
+                .andThen(
+                    Commands.parallel(
+                        new StraightDriveToPose(notePose, drive),
+                        arm.toSetpoint(
+                            new Rotation2d(
+                                -9000) // TODO: Add the correct setpoint for the two note auto
+                            )))
+                .andThen(
+                    Commands.parallel(intake.intake(), indexer.setIndexerPercentVelocity(0.25)))
+                .andThen(new StraightDriveToPose(getSpeakerTargetPose().get(), drive))
+                .andThen(
+                    arm.toSetpoint(
+                        new Rotation2d(
+                            shooter.calculateShootingAngle(
+                                    drive
+                                        .getPose()
+                                        .getTranslation()
+                                        .getDistance(
+                                            AllianceFlipUtil.apply(
+                                                FieldConstants.Speaker.centerSpeakerOpening
+                                                    .toTranslation2d())))
+                                - ArmConstants.HORIZONTAL_OFFSET.getRadians())))
+                .andThen(shooter.shoot(ShooterConstants.SHOOTER_READY_VELOCITY_RAD_PER_SEC)));
   }
 }
