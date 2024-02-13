a3798abdc87b60413300d6e7f17ccd9bab56d0e9
Bryan Duerk <bryan.duerk@gmail.com>
2024-02-12 19:44:33 -0500

    Put the note camera info into robot state for sharing

--- src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
+++ src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems.pixy2;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.robotstate.RobotState;
 import org.littletonrobotics.junction.Logger;
 
 public class Pixy2 extends SubsystemBase {
   private final Pixy2IO io;
   private final Pixy2IOInputsAutoLogged inputs = new Pixy2IOInputsAutoLogged();
+
+  /** Used to share robot state across subsystems */
+  private final RobotState robotState = RobotState.getInstance();
 
   public Pixy2(Pixy2IO io) {
     this.io = io;
@@ -15,6 +19,11 @@ public class Pixy2 extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Pixy2", inputs);
+
+    // Currently we just share presence of a note and the angle to the note
+    // Distance to the note is TBD
+    robotState.seeNote = inputs.seesNote;
+    robotState.noteAngle = inputs.angle;
   }
 
   public double getAge() {
