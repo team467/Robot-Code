5da9bf6011f13cde1c8c3d2fb754a0a904c28602
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-02-16 18:09:33 -0500

    fix offsets

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -47,10 +47,10 @@ public class DriveConstants {
         WHEEL_DIAMETER = Units.inchesToMeters(4);
         ABSOLUTE_ANGLE_OFFSET =
             new Rotation2d[] {
-              Rotation2d.fromDegrees(277.4),
-              Rotation2d.fromDegrees(46.6),
-              Rotation2d.fromDegrees(89.7),
-              Rotation2d.fromDegrees(-137.7),
+              Rotation2d.fromDegrees(136.75),
+              Rotation2d.fromDegrees(-47.11),
+              Rotation2d.fromDegrees(5.80),
+              Rotation2d.fromDegrees(177.01),
             };
         DRIVE_GEAR_RATIO = new GearRatio(6.12, 1);
         TURN_GEAR_RATIO = new GearRatio(12.8, 1);
