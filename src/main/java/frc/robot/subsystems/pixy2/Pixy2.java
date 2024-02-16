16ee2a077889ea081af66d63aa08d00dcf59c7db
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2024-02-16 18:33:22 -0500

    Updated Robot State for Pixy2.

--- src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
+++ src/main/java/frc/robot/subsystems/pixy2/Pixy2.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems.pixy2;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.robotstate.RobotState;
 import org.littletonrobotics.junction.Logger;
 
 public class Pixy2 extends SubsystemBase {
   private final Pixy2IO io;
   private final Pixy2IOInputsAutoLogged inputs = new Pixy2IOInputsAutoLogged();
+  private final RobotState robotState = RobotState.getInstance();
 
   public Pixy2(Pixy2IO io) {
     this.io = io;
@@ -15,6 +17,7 @@ public class Pixy2 extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Pixy2", inputs);
+    robotState.seeNote = this.seesNote();
   }
 
   public double getAge() {
