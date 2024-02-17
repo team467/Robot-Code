f5dcbbab5086c9966d7fe4edcb92532bb51c0d28
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-02-17 13:45:07 -0500

    add faceNote command

--- src/main/java/frc/robot/commands/auto/Autos.java
+++ src/main/java/frc/robot/commands/auto/Autos.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import frc.lib.utils.AllianceFlipUtil;
 import frc.robot.FieldConstants;
 import frc.robot.subsystems.arm.Arm;
@@ -12,6 +13,7 @@ import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.pixy2.Pixy2;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterConstants;
 import java.util.Set;
@@ -24,6 +26,7 @@ public class Autos {
   private final Indexer indexer;
   private final Arm arm;
   private final Intake intake;
+  private final Pixy2 pixy2;
   private Translation2d noteTranslation;
   private Translation2d secondNoteTranslation;
   private Translation2d thirdNoteTranslation;
@@ -31,12 +34,13 @@ public class Autos {
   private final Translation2d speaker =
       AllianceFlipUtil.apply(FieldConstants.Speaker.centerSpeakerOpening.toTranslation2d());
 
-  public Autos(Drive drive, Shooter shooter, Indexer indexer, Arm arm, Intake intake) {
+  public Autos(Drive drive, Shooter shooter, Indexer indexer, Arm arm, Intake intake, Pixy2 pixy2) {
     this.drive = drive;
     this.shooter = shooter;
     this.indexer = indexer;
     this.arm = arm;
     this.intake = intake;
+    this.pixy2 = pixy2;
   }
 
   public enum StartingPosition {
@@ -103,6 +107,25 @@ public class Autos {
     return Commands.defer(
         () -> new StraightDriveToPose(new Pose2d(targetTranslation, targetRotation.get()), drive),
         Set.of(drive));
+  }
+
+  public Command faceNote() {
+    if(pixy2.seesNote()) {
+      return Commands.defer(
+              () ->
+                      new ProxyCommand(
+                              () ->
+                                      new StraightDriveToPose(
+                                              new Pose2d(
+                                                      drive.getPose().getTranslation(),
+                                                      drive
+                                                              .getRotation()
+                                                              .plus(Rotation2d.fromDegrees(pixy2.getAngle()))),
+                                              drive)),
+              Set.of(drive));
+    } else {
+      return Commands.none();
+    }
   }
 
   private Command alignArm() {
