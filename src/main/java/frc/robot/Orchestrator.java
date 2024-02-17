779a3603f43f532e233e5f47f9bcee629130c600
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2024-02-17 16:16:26 -0500

    Added the use of robot states in Orchestrator for intaking.

--- src/main/java/frc/robot/Orchestrator.java
+++ src/main/java/frc/robot/Orchestrator.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.pixy2.Pixy2;
+import frc.robot.subsystems.robotstate.RobotState;
 import frc.robot.subsystems.shooter.Shooter;
 
 public class Orchestrator {
@@ -17,15 +18,23 @@ public class Orchestrator {
   private final Shooter shooter;
   private final Pixy2 pixy2;
   private final Arm arm;
+  private final RobotState robotState;
 
   public Orchestrator(
-      Drive drive, Intake intake, Indexer indexer, Shooter shooter, Pixy2 pixy2, Arm arm) {
+      Drive drive,
+      Intake intake,
+      Indexer indexer,
+      Shooter shooter,
+      Pixy2 pixy2,
+      Arm arm,
+      RobotState robotState) {
     this.drive = drive;
     this.intake = intake;
     this.indexer = indexer;
     this.shooter = shooter;
     this.pixy2 = pixy2;
     this.arm = arm;
+    this.robotState = robotState;
   }
 
   public Command shootBasic() {
@@ -35,27 +44,27 @@ public class Orchestrator {
                 // Turn towards speaker.
                 ),
             Commands.waitUntil(shooter::ShooterSpeedIsReady).withTimeout(2),
-            indexer.setIndexerPercentVoltage(1),
+            indexer.setVolts(1),
             Commands.waitUntil(() -> !indexer.getLimitSwitchPressed()).withTimeout(2),
-            Commands.parallel(indexer.setIndexerPercentVoltage(0), shooter.manualShoot(0)))
+            Commands.parallel(indexer.setVolts(0), shooter.manualShoot(0)))
         .onlyIf(indexer::getLimitSwitchPressed);
   }
 
   public Command intakeBasic() {
     return Commands.sequence(
-        indexer.setIndexerPercentVoltage(4),
+        indexer.setVolts(4),
         arm.toSetpoint(new Rotation2d()), // TODO: Make setPoint for pickup position.
         Commands.waitUntil(arm::atSetpoint).withTimeout(2),
-        intake.intake());
+        intake.intake().until(() -> robotState.hasNote));
   }
 
   // Intakes after seeing note with Pixy2.
   public Command visionIntake() {
     return Commands.sequence(
-        indexer.setIndexerPercentVoltage(4),
+        indexer.setVolts(4),
         arm.toSetpoint(new Rotation2d()), // TODO: Make setpoint for pickup position.
         Commands.waitUntil(() -> arm.atSetpoint() && pixy2.seesNote()).withTimeout(2),
-        intake.intake());
+        intake.intake().until(() -> robotState.hasNote));
   }
 
   public Command climb() {
@@ -66,11 +75,11 @@ public class Orchestrator {
     return Commands.parallel(
         // arm.toSetpoint(new Rotation2d()), //TODO Make setPoint for pickup position.
         // Commands.waitUntil(arm::atSetpoint).withTimeout(2),
-        shooter.manualShoot(-5), indexer.setIndexerVoltage(-5.0), intake.release());
+        shooter.manualShoot(-5), indexer.setVolts(-5.0), intake.release());
   }
 
   public Command expelShooter() {
-    return Commands.parallel(shooter.manualShoot(-5.0), indexer.setIndexerVoltage(-5.0));
+    return Commands.parallel(shooter.manualShoot(-5.0), indexer.setVolts(-5.0));
   }
 
   public Command expelIntake() {
