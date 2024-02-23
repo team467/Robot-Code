5bf62cb9f09e306de60cce3f2e680e3b4f0a44fd
rafiaaleem5 <rafiaaleem5@gmail.com>
2024-02-23 17:09:07 -0500

    added a new boolean relating to limitswitch

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -7,6 +7,7 @@ public interface ClimberIO {
   class ClimberIOInputs {
     public double motorPercentOutput = 0.0;
     public double motorVoltage = 0.0;
+    public boolean limitSwitchPressed = false;
   }
 
   default void updateInput(ClimberIOInputs inputs) {}
