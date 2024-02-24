75b14c33dd1f6f095ac5951a7cd53f079d1765dd
rafiaaleem5 <rafiaaleem5@gmail.com>
2024-02-24 14:03:51 -0500

    Edited the if else statement fo the limit switch

--- src/main/java/frc/robot/subsystems/climber/ClimberIOTalonSRX.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOTalonSRX.java
@@ -16,15 +16,15 @@ public class ClimberIOTalonSRX implements ClimberIO {
   public void updateInput(ClimberIOInputs inputs) {
     inputs.motorPercentOutput = motor.getMotorOutputPercent();
     inputs.motorVoltage = motor.getMotorOutputVoltage();
-    inputs.limitSwitchPressed = (motor.isFwdLimitSwitchClosed() == 1
-        || motor.isRevLimitSwitchClosed() == 1);
+    inputs.limitSwitchPressed =
+        (motor.isFwdLimitSwitchClosed() == 1 || motor.isRevLimitSwitchClosed() == 1);
   }
 
   @Override
   public void setMotorOutputPercent(double percentOutput) {
-    if (motor.isFwdLimitSwitchClosed() == 1 || motor.isRevLimitSwitchClosed() == 1
-      && percentOutput < 0) {
-      motor.set(TalonSRXControlMode.PercentOutput, 0.0);  
+    if (motor.isFwdLimitSwitchClosed() == 1
+        || motor.isRevLimitSwitchClosed() == 1 && percentOutput < 0) {
+      motor.set(TalonSRXControlMode.PercentOutput, 0.0);
     } else {
       motor.set(TalonSRXControlMode.PercentOutput, percentOutput);
     }
