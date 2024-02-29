9895d95c5a2b10273574cec168e7ed59fdcf2cb5
Anshu Adiga <aradig26@students.shrewsbury.k12.ma.us>
2024-02-28 22:41:06 -0500

    format

--- src/main/java/frc/robot/commands/auto/Autos.java
+++ src/main/java/frc/robot/commands/auto/Autos.java
@@ -1,24 +1,12 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import frc.lib.utils.AllianceFlipUtil;
 import frc.robot.FieldConstants;
 import frc.robot.Orchestrator;
-import frc.robot.subsystems.arm.Arm;
-import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.indexer.Indexer;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.pixy2.Pixy2;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.shooter.ShooterConstants;
-import java.util.Set;
-import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 public class Autos {
@@ -72,52 +60,49 @@ public class Autos {
     Logger.recordOutput("Autos/NotePositions/2", thirdNoteTranslation);
   }
 
-
   public Command oneNoteAuto() {
     return orchestrator.fullAlignShootSpeaker();
   }
 
   public Command twoNoteAuto(StartingPosition position) {
     setNotePositions(position);
-    return orchestrator.fullAlignShootSpeaker()
-            .andThen(
+    return orchestrator
+        .fullAlignShootSpeaker()
+        .andThen(
             Commands.parallel(
-                    orchestrator.driveToNote(noteTranslation),
-                    orchestrator.intakeBasic()))
+                orchestrator.driveToNote(noteTranslation), orchestrator.intakeBasic()))
         .andThen(orchestrator.fullAlignShootSpeaker());
   }
 
   public Command threeNoteAuto(StartingPosition position) {
     setNotePositions(position);
-    return orchestrator.fullAlignShootSpeaker()
-            .andThen(
-                    Commands.parallel(
-                            orchestrator.driveToNote(noteTranslation),
-                            orchestrator.intakeBasic()))
-            .andThen(orchestrator.fullAlignShootSpeaker())
+    return orchestrator
+        .fullAlignShootSpeaker()
         .andThen(
             Commands.parallel(
-                            orchestrator.driveToNote(secondNoteTranslation),
-                    orchestrator.intakeBasic())
+                orchestrator.driveToNote(noteTranslation), orchestrator.intakeBasic()))
+        .andThen(orchestrator.fullAlignShootSpeaker())
+        .andThen(
+            Commands.parallel(
+                    orchestrator.driveToNote(secondNoteTranslation), orchestrator.intakeBasic())
                 .andThen(orchestrator.fullAlignShootSpeaker()));
   }
 
   public Command fourNoteAuto(StartingPosition position) {
     setNotePositions(position);
-    return orchestrator.fullAlignShootSpeaker()
-            .andThen(
-                    Commands.parallel(
-                            orchestrator.driveToNote(noteTranslation),
-                            orchestrator.intakeBasic()))
-            .andThen(orchestrator.fullAlignShootSpeaker())
-            .andThen(
-                    Commands.parallel(
-                                    orchestrator.driveToNote(secondNoteTranslation),
-                                    orchestrator.intakeBasic())
-                            .andThen(orchestrator.fullAlignShootSpeaker()))
+    return orchestrator
+        .fullAlignShootSpeaker()
         .andThen(
             Commands.parallel(
-                            orchestrator.driveToNote(thirdNoteTranslation),orchestrator.intakeBasic()
-                    )).andThen(orchestrator.fullAlignShootSpeaker());
+                orchestrator.driveToNote(noteTranslation), orchestrator.intakeBasic()))
+        .andThen(orchestrator.fullAlignShootSpeaker())
+        .andThen(
+            Commands.parallel(
+                    orchestrator.driveToNote(secondNoteTranslation), orchestrator.intakeBasic())
+                .andThen(orchestrator.fullAlignShootSpeaker()))
+        .andThen(
+            Commands.parallel(
+                orchestrator.driveToNote(thirdNoteTranslation), orchestrator.intakeBasic()))
+        .andThen(orchestrator.fullAlignShootSpeaker());
   }
 }
