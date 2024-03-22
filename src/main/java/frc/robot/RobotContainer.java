d2b86c7f875c34b0d82341c50ee217c2acc0b49a
Adam Nativ <admlvntv@gmail.com>
2024-03-22 18:16:17 -0400

    remove pixy

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -42,7 +42,6 @@ import frc.robot.subsystems.intake.IntakeIOPhysical;
 import frc.robot.subsystems.led.Leds;
 import frc.robot.subsystems.pixy2.Pixy2;
 import frc.robot.subsystems.pixy2.Pixy2IO;
-import frc.robot.subsystems.pixy2.Pixy2IOPhysical;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterIO;
 import frc.robot.subsystems.shooter.ShooterIOPhysical;
@@ -126,7 +125,7 @@ public class RobotContainer {
           intake = new Intake(new IntakeIOPhysical());
           shooter = new Shooter(new ShooterIOPhysical());
           leds = new Leds();
-          pixy2 = new Pixy2(new Pixy2IOPhysical());
+          // pixy2 = new Pixy2(new Pixy2IOPhysical());
           //          climber = new Climber(new ClimberIOSparkMax());
         }
 
