6e022cc9ab446bde01b45a27155285466abbe644
Aradig26 <aradig26@students.shrewsbury.k12.ma.us>
2024-04-05 07:22:27 -0400

    increase indexer speed

--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -34,7 +34,7 @@ public class IndexerConstants {
         INDEXER_BACKWARD_VOLTAGE = 0.0;
         INDEXER_LIMIT_SWITCH_ID = 13;
         WHEEL_DIAMETER = 4;
-        INDEX_SPEED = new TunableNumber("Indexer/IndexSpeed", 0.8);
+        INDEX_SPEED = new TunableNumber("Indexer/IndexSpeed", 1);
         INDEXER_GEAR_RATIO = new GearRatio(1.5, 1);
         BACKUP_SPEED = -0.3;
         BACKUP_TIME = 0.2;
