19cae208eb959501294960aa2705f2e1bf9b5360
Bhoomi Prashanth <bhoomiprashanth10@gmail.com>
2025-01-11 15:22:02 -0500

    added arm extension and retraction

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffector.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffector.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.algae;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeEffector extends SubsystemBase {
@@ -10,5 +11,13 @@ public class AlgaeEffector extends SubsystemBase {
 
   public AlgaeEffector(AlgaeEffectorIO io) {
     this.io = io;
+  }
+
+  public Command extendArm(double length) {
+    return this.runOnce(() -> io.setPivotVolts(AlgaeEffectorConstants.EXTEND_VOLTAGE));
+  }
+
+  public Command retractArm(double length) {
+    return this.runOnce(() -> io.setPivotVolts(AlgaeEffectorConstants.RETRACT_VOLTAGE));
   }
 }
