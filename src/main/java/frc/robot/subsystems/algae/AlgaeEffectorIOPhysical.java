f44b96e6718945b9f3cf2a5ca2e74fd4a35ed390
Bhoomi Prashanth <bhoomiprashanth10@gmail.com>
2025-01-11 15:21:33 -0500

    Created limit switches

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
@@ -7,6 +7,8 @@ public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
 
   private final SparkMax pivotMotor;
   private final SparkMax removalMotor;
+  private final SparkLimitSwitch pivotMotorExtendLimitSwitch;
+  private final SparkLimitSwitch pivotMotorStoweLimitSwitch;
   // TODO: Move to schematic
   private static final int PIVOT_ID = 1;
   private static final int REMOVAL_ID = 2;
@@ -14,6 +16,9 @@ public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
   public AlgaeEffectorIOPhysical() {
     pivotMotor = new SparkMax(PIVOT_ID, MotorType.kBrushless);
     removalMotor = new SparkMax(REMOVAL_ID, MotorType.kBrushless);
+
+    pivotMotorExtendLimitSwitch = pivotMotor.getForwardLimitSwitch();
+    pivotMotorStoweLimitSwitch = pivotMotor.getReverseLimitSwitch();
   }
 
   public void setRemovalVolts(double volts) {
