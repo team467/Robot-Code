e7cf617aa5eaf6a6a9ebad1dd64f6d1990fb5c6d
pgund <183660136+pgund@users.noreply.github.com>
2025-01-11 04:49:02 -0500

    blank constants for elevator

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.elevator;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import frc.lib.utils.TunableNumber;
 import frc.robot.Constants;
