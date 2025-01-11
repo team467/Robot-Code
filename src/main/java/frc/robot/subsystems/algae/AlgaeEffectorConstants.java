df0e96ecb21a1413c13af4f1f40aa0c7e988a8ed
Bhoomi Prashanth <bhoomiprashanth10@gmail.com>
2025-01-10 20:21:15 -0500

    Create first version of algae-effector with two motors

---
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorConstants.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.algae;
+
+public class AlgaeEffectorConstants {
+  public static final double STOW_ANGLE = 0.0;
+  public static final double PIVOT_ANGLE = 0.0;
+  public static final double SCORE_ANGLE = 0.0;
+
+  public static final double INTAKE_VOLTAGE = 6.0;
+  public static final double SHOOTING_VOLTAGE = -6.0;
+
+  public static double PIVOT_ANGLE_KS = 1.0;
+  public static double PIVOT_KD = 1.0;
+  public static double PIVOT_KP = 1.0;
+  public static double PIVOT_KV = 1.0;
+}
---
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.algae;
+
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
+
+  // two motors (pivot, rotate)
+  private final SparkMax pivotMotor;
+  private final SparkMax intakeMotor;
+  // In schematic we add
+  private static final int PIVOT_ID = 1;
+  private static final int INTAKE = 1;
+
+  public AlgaeEffectorIOPhysical() {
+    pivotMotor = new SparkMax(PIVOT_ID, MotorType.kBrushless);
+    intakeMotor = new SparkMax(INTAKE, MotorType.kBrushless);
+  }
+
+  public void setIntakeVolts(double volts) {
+    pivotMotor.setVoltage(volts);
+  }
+
+  public void setPivotVolts(double volts) {
+    intakeMotor.setVoltage(volts);
+  }
+}
