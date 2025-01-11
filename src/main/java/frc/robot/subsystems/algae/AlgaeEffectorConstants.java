490ca50c85f4efd7ec198d3d8ff8464639833350
Bhoomi Prashanth <bhoomiprashanth10@gmail.com>
2025-01-11 13:26:34 -0500

    Changed 'intake' to 'removal'

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorConstants.java
@@ -2,11 +2,9 @@ package frc.robot.subsystems.algae;
 
 public class AlgaeEffectorConstants {
   public static final double STOW_ANGLE = 0.0;
-  public static final double PIVOT_ANGLE = 0.0;
-  public static final double SCORE_ANGLE = 0.0;
+  public static final double REMOVAL_ANGLE = 0.0;
 
-  public static final double INTAKE_VOLTAGE = 6.0;
-  public static final double SHOOTING_VOLTAGE = -6.0;
+  public static final double REMOVAL_VOLTAGE = 6.0;
 
   public static double PIVOT_ANGLE_KS = 1.0;
   public static double PIVOT_KD = 1.0;
--- src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
@@ -6,21 +6,21 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
 
   private final SparkMax pivotMotor;
-  private final SparkMax intakeMotor;
+  private final SparkMax removalMotor;
   // TODO: Move to schematic
   private static final int PIVOT_ID = 1;
-  private static final int INTAKE = 1;
+  private static final int REMOVAL = 1;
 
   public AlgaeEffectorIOPhysical() {
     pivotMotor = new SparkMax(PIVOT_ID, MotorType.kBrushless);
-    intakeMotor = new SparkMax(INTAKE, MotorType.kBrushless);
+    removalMotor = new SparkMax(REMOVAL, MotorType.kBrushless);
   }
 
-  public void setIntakeVolts(double volts) {
+  public void setRemovalVolts(double volts) {
     pivotMotor.setVoltage(volts);
   }
 
   public void setPivotVolts(double volts) {
-    intakeMotor.setVoltage(volts);
+    removalMotor.setVoltage(volts);
   }
 }
