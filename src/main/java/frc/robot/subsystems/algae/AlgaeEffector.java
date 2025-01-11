43a4f05edd2f3c117c9bae585a3e64a7ed5d0a8a
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-01-11 15:45:59 -0500

    arm extension and retraction time outs and stop commands

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffector.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffector.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems.algae;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 
 public class AlgaeEffector extends SubsystemBase {
   private final AlgaeEffectorIO io;
@@ -15,10 +15,18 @@ public class AlgaeEffector extends SubsystemBase {
   }
 
   public Command extendArm(double length) {
-    return this.runOnce(() -> io.setPivotVolts(AlgaeEffectorConstants.EXTEND_VOLTAGE));
+    return Commands.startEnd(
+            () -> io.setPivotVolts(AlgaeEffectorConstants.EXTEND_VOLTAGE),
+            () -> io.setPivotVolts(AlgaeEffectorConstants.ZERO_VOLTAGE),
+            this)
+        .withTimeout(2);
   }
 
   public Command retractArm(double length) {
-    return this.runOnce(() -> io.setPivotVolts(AlgaeEffectorConstants.RETRACT_VOLTAGE));
+    return Commands.startEnd(
+            () -> io.setPivotVolts(AlgaeEffectorConstants.RETRACT_VOLTAGE),
+            () -> io.setPivotVolts(AlgaeEffectorConstants.ZERO_VOLTAGE),
+            this)
+        .withTimeout(2);
   }
 }
