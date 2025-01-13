31b26217d9bd3c3b91303e5013a8b7310aa43963
pgund <183660136+pgund@users.noreply.github.com>
2025-01-12 19:23:09 -0500

    most of Elevator.java

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -3,18 +3,18 @@ package frc.robot.subsystems.elevator;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ElevatorIO {
-    @AutoLog
-    class ElevatorIOInputs {
-        public double positionMeters = 0.0;
-        public double velocityMetersPerSec = 0.0;
-        public double elevatorAppliedVolts = 0.0;
-        public double[] elevatorCurrentAmps = new double[] {};
-        public boolean limitSwitchPressed = false;
-    }
+  @AutoLog
+  class ElevatorIOInputs {
+    public double positionMeters = 0.0;
+    public double velocityMetersPerSec = 0.0;
+    public double elevatorAppliedVolts = 0.0;
+    public double[] elevatorCurrentAmps = new double[] {};
+    public boolean limitSwitchPressed = false;
+  }
 
-    default void updateInputs(ElevatorIOInputs inputs) {}
+  default void updateInputs(ElevatorIOInputs inputs) {}
 
-    default void setVoltage(double volts) {}
+  default void setVoltage(double volts) {}
 
-    default void resetPosition() {}
+  default void resetPosition() {}
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSparkMAX.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSparkMAX.java
@@ -1,4 +1,3 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorIOSparkMAX {
-}
+public class ElevatorIOSparkMAX {}
