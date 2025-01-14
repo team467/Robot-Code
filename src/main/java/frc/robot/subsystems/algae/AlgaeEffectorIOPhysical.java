cdc755aa259dfbbc447466a3bc628c5dc3acd160
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-01-13 20:26:15 -0500

    addedupdate inputs, and auto log

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOPhysical.java
@@ -1,15 +1,23 @@
 package frc.robot.subsystems.algae;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.RelativeEncoder.*;
 import com.revrobotics.spark.*;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
 
+  // motors
   private final SparkMax pivotMotor;
   private final SparkMax removalMotor;
 
-  private final SparkLimitSwitch pivotMotorExtendLimitSwitch;
-  private final SparkLimitSwitch pivotMotorStowLimitSwitch;
+  // limit switches
+  // private final SparkLimitSwitch pivotMotorExtendLimitSwitch;
+  // private final SparkLimitSwitch pivotMotorStowLimitSwitch;
+
+  // motor encoders
+  private final RelativeEncoder pivotMotorEncoder;
+  private final RelativeEncoder removalMotorEncoder;
 
   // TODO: Move to schematic
   private static final int PIVOT_ID = 1;
@@ -19,8 +27,11 @@ public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
     pivotMotor = new SparkMax(PIVOT_ID, MotorType.kBrushless);
     removalMotor = new SparkMax(REMOVAL_ID, MotorType.kBrushless);
 
-    pivotMotorExtendLimitSwitch = pivotMotor.getForwardLimitSwitch();
-    pivotMotorStowLimitSwitch = pivotMotor.getReverseLimitSwitch();
+    // pivotMotorExtendLimitSwitch = pivotMotor.getForwardLimitSwitch();
+    // pivotMotorStowLimitSwitch = pivotMotor.getReverseLimitSwitch();
+
+    pivotMotorEncoder = pivotMotor.getEncoder();
+    removalMotorEncoder = removalMotor.getEncoder();
   }
 
   public void setRemovalVolts(double volts) {
@@ -29,5 +40,15 @@ public class AlgaeEffectorIOPhysical implements AlgaeEffectorIO {
 
   public void setPivotVolts(double volts) {
     pivotMotor.setVoltage(volts);
+  }
+
+  public void updateInputs(AlgaeEffectorIOInputs inputs) {
+    inputs.pivotVolts = pivotMotor.getBusVoltage() * pivotMotor.getAppliedOutput();
+    inputs.removalVolts = removalMotor.getBusVoltage() * removalMotor.getAppliedOutput();
+    inputs.pivotAmps = pivotMotor.getOutputCurrent();
+    inputs.removalAmps = removalMotor.getOutputCurrent();
+    inputs.pivotVelocity = pivotMotorEncoder.getVelocity();
+    inputs.removalVelocity = removalMotorEncoder.getVelocity();
+    inputs.pivotPosition = pivotMotorEncoder.getPosition();
   }
 }
