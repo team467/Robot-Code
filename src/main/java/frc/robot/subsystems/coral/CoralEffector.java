0a00f3d1afd93d96b15e57867610f454f3de90ba
Rshar2027 <rshar272k@gmail.com>
2025-01-18 17:18:09 -0500

    Effector Almost Finished

--- src/main/java/frc/robot/subsystems/coral/CoralEffector.java
+++ src/main/java/frc/robot/subsystems/coral/CoralEffector.java
@@ -1,20 +1,39 @@
 package frc.robot.subsystems.coral;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotState;
 
 public class Effector extends SubsystemBase {
 
   private final EffectorIO io;
+  //private final EffectorIOInputsAutoLogged inputs;
 
   private final RobotState robotState = RobotState.getInstance();
   private boolean PIDMode = false;
   private double currentVelocitySetpoint;
 
-  // private SimpleMotorFeedforward shooterFeedforward =SF;
-  // new
+  private SimpleMotorFeedforward shooterDropforward
 
-  public Effector(EffectorIO io) {
+
+
+
+
+
+ /* Not Sure To Use AutoLogged
+
+ public Effector(EffectorIO io) {
+
     this.io = io;
+    this.inputs = new EffectorIOInputsAutoLogged();
   }
+  */
+
+  public Effector(EffectorIO io){
+    this.io = io;
+
+  }
+
+
+
 }
