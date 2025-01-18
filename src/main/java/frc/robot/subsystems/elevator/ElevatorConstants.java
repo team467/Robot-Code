01398cf27b8aef86342d543831e90cf66e5f6bee
ShayanHasHackedThis <shayansteel117@gmail.com>
2025-01-17 21:13:59 -0500

    added sparkmax for elevator :)

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -14,6 +14,8 @@ public class ElevatorConstants {
   public static final TunableNumber MAX_ACCELERATION;
   public static final double STOW;
   public static final double TOLERANCE;
+  public static final int elevatorCanId = 0;
+  public static final int elevatorCurrentLimit = 5;
 
   static {
     switch (Constants.getRobot()) {
