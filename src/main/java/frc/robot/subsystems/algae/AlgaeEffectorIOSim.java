e7ecc8cf70365bfbf0ea2208dc954cfbd7da73c8
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-01-18 20:09:19 -0500

    removed unused imports

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOSim.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffectorIOSim.java
@@ -1,44 +1,38 @@
 package frc.robot.subsystems.algae;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.units.AngularVelocityUnit;
 import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import edu.wpi.first.wpilibj.simulation.EncoderSim;
 
 public class AlgaeEffectorIOSim implements AlgaeEffectorIO {
-  
+
   // motors
   private final DCMotorSim pivotMotorSim;
   private final DCMotorSim removalMotorSim;
 
   private double pivotMotorSimVolts;
   private double removalMotorSimVolts;
-  
+
   // constants needed to set up DC motor system, change as needed
-  // random Jkg value, change if needed 
+  // random Jkg value, change if needed
   private static final double JKgMetersSquared = 0.025;
-  // random gearing value, change if needed 
+  // random gearing value, change if needed
   private static final double gearing = 4.71;
   public static final DCMotor algaeGearbox = DCMotor.getNeoVortex(1);
- 
-  // limit switches? 
+
+  // limit switches?
 
   public AlgaeEffectorIOSim() {
-    pivotMotorSim = 
+    pivotMotorSim =
         new DCMotorSim(
-          LinearSystemId.createDCMotorSystem(algaeGearbox, JKgMetersSquared, gearing),
-          algaeGearbox);
-    
-    removalMotorSim = 
+            LinearSystemId.createDCMotorSystem(algaeGearbox, JKgMetersSquared, gearing),
+            algaeGearbox);
+
+    removalMotorSim =
         new DCMotorSim(
-          LinearSystemId.createDCMotorSystem(algaeGearbox, JKgMetersSquared, gearing),
-          algaeGearbox);
+            LinearSystemId.createDCMotorSystem(algaeGearbox, JKgMetersSquared, gearing),
+            algaeGearbox);
   }
 
   public void setRemovalVolts(double volts) {
