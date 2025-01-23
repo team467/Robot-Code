52f63cbe447655f77383942c14c97d339ce72b3b
ratdw <dhriti.krish10@gmail.com>
2025-01-22 19:54:00 -0500

    pattern bank created

--- src/main/java/frc/robot/subsystems/leds/LedPatterns.java
+++ src/main/java/frc/robot/subsystems/leds/LedPatterns.java
@@ -1,36 +1,29 @@
 package frc.robot.subsystems.leds;
 
-import static edu.wpi.first.units.Units.Centimeters;
-import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Percent;
-import static edu.wpi.first.units.Units.Second;
 
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
+import java.util.Map;
 
-public class LedPatterns {
+public enum LedPatterns {
+  SOLID_BLUE(LEDPattern.solid(Color.kBlue)),
+  SOLID_RED(LEDPattern.solid(Color.kRed)),
+  SOLID_ORANGE_RED(LEDPattern.solid(Color.kOrangeRed)),
+  SOLID_WHITE(LEDPattern.solid(Color.kWhite)),
+  SCROLLING_RAINBOW(
+      LEDPattern.rainbow(255, 128)
+          .scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedConstants.LED_SPACING)),
+  CONTINUOUS_GRADIENT_FROM_RED_TO_BLUE(
+      LEDPattern.gradient(LEDPattern.GradientType.kContinuous, Color.kRed, Color.kBlue)),
+  DISCONTINUOUS_GRADIENT_FROM_HOTPINK_TO_LIME(
+      LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, Color.kHotPink, Color.kLime)),
+  STEPS(LEDPattern.steps(Map.of(0, Color.kRed, 0.5, Color.kBlue))),
+  BREATH(LEDPattern.solid(Color.kHotPink));
 
-  public static final Distance LED_SPACING = Meters.of(1 / 60.0);
-  public static final LEDPattern BASE =
-      LEDPattern.gradient(LEDPattern.GradientType.kContinuous, Color.kRed, Color.kBlue)
-          .scrollAtAbsoluteSpeed(Centimeters.per(Second).of(15), LED_SPACING);
-  LEDPattern pattern = BASE.scrollAtRelativeSpeed(Percent.per(Second).of(25));
-  LEDPattern absolute = BASE.scrollAtAbsoluteSpeed(Centimeters.per(Second).of(12.5), LED_SPACING);
-  public static final LEDPattern BREATH = LEDPattern.solid(Color.kWhiteSmoke);
-  public static final LEDPattern ESTOPPED = LEDPattern.solid(Color.kRed);
-  public static final LEDPattern AUTO_FINISHED = LEDPattern.solid(Color.kOrange);
-  public static final LEDPattern AUTONOMOUS =
-      LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LED_SPACING);
-  public static final LEDPattern BLUE_ALLIANCE = LEDPattern.solid(Color.kBlue);
-  public static final LEDPattern RED_ALLIANCE = LEDPattern.solid(Color.kRed);
-  public static final LEDPattern LOW_BATTERY_ALERT = LEDPattern.solid(Color.kAquamarine);
-  public static final LEDPattern DISABLED = LEDPattern.solid(Color.kLavender);
-  public static final LEDPattern OFF = LEDPattern.solid(Color.kDarkViolet);
-  public static final LEDPattern DEFAULT = LEDPattern.solid(Color.kDeepPink);
-  public static final LEDPattern BASE2 =
-      LEDPattern.gradient(
-          LEDPattern.GradientType.kDiscontinuous, Color.kDarkTurquoise, Color.kDarkViolet);
-  LEDPattern LedPattern = BASE2.breathe(Second.of(2));
+  public final LEDPattern pattern;
+
+  private LedPatterns(LEDPattern pattern) {
+    this.pattern = pattern;
+  }
 }
