20e926af904d4023a1c623e4266fdecc5daad026
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-01-26 14:36:53 -0500

    minor change

--- src/main/java/frc/robot/subsystems/algae/AlgaeEffector.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeEffector.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotState;
-
 import org.littletonrobotics.junction.Logger;
 
 public class AlgaeEffector extends SubsystemBase {
