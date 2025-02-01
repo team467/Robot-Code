af612b87e5a32d0121be3041a22e0f640a6f632e
Adam Nativ <admlvntv@gmail.com>
2025-02-01 14:35:51 -0500

    update drive can ids

--- src/main/java/frc/robot/Schematic.java
+++ src/main/java/frc/robot/Schematic.java
@@ -54,14 +54,14 @@ public class Schematic {
         pigeonCanId = 17;
 
         frontLeftDriveCanId = 1;
-        backLeftDriveCanId = 3;
-        frontRightDriveCanId = 5;
-        backRightDriveCanId = 7;
+        backLeftDriveCanId = 7;
+        frontRightDriveCanId = 3;
+        backRightDriveCanId = 5;
 
         frontLeftTurnCanId = 2;
-        backLeftTurnCanId = 4;
-        frontRightTurnCanId = 6;
-        backRightTurnCanId = 8;
+        backLeftTurnCanId = 8;
+        frontRightTurnCanId = 4;
+        backRightTurnCanId = 6;
 
         frontLeftAbsoluteEncoderCanId = 18;
         backLeftAbsoluteEncoderCanId = 21;
