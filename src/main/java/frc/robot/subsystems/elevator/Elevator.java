bde603e521f9da2ea73c8ef04545a386831f69e4
Adam Nativ <admlvntv@gmail.com>
2025-02-03 20:07:11 -0500

    Replace Profiled PID with State Space
    See https://github.com/wpilibsuite/allwpilib/blob/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/statespaceelevator/Robot.java

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,9 +1,18 @@
 package frc.robot.subsystems.elevator;
 
-import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.LinearQuadraticRegulator;
+import edu.wpi.first.math.estimator.KalmanFilter;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.LinearSystemLoop;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,16 +24,14 @@ public class Elevator extends SubsystemBase {
   private final ElevatorIO io;
   private final ElevatorIOInputsAutoLogged inputs;
 
-  private ElevatorFeedforward feedforward =
-      new ElevatorFeedforward(
-          ElevatorConstants.KS.get(), ElevatorConstants.KG.get(), ElevatorConstants.KV.get());
-  private ProfiledPIDController feedback =
-      new ProfiledPIDController(
-          ElevatorConstants.KP.get(),
-          0,
-          ElevatorConstants.KD.get(),
-          new TrapezoidProfile.Constraints(
-              ElevatorConstants.MAX_VELOCITY.get(), ElevatorConstants.MAX_ACCELERATION.get()));
+  private TrapezoidProfile profile;
+  private TrapezoidProfile.State lastProfiledReference = new TrapezoidProfile.State();
+  private TrapezoidProfile.State goal;
+  private final LinearSystem<N2, N1, N2> elevatorPlant;
+  private final KalmanFilter<N2, N1, N1> observer;
+  private final LinearQuadraticRegulator<N2, N1, N1> controller;
+  private final LinearSystemLoop<N2, N1, N1> loop;
+
   private boolean feedbackMode = true;
 
   @AutoLogOutput private boolean isCalibrated = false;
@@ -35,11 +42,59 @@ public class Elevator extends SubsystemBase {
     this.inputs = new ElevatorIOInputsAutoLogged();
     io.updateInputs(inputs);
 
-    feedback.disableContinuousInput();
-    feedback.reset(inputs.positionMeters);
-    feedback.setGoal(inputs.positionMeters);
+    profile =
+        new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(
+                ElevatorConstants.MAX_VELOCITY.get(), ElevatorConstants.MAX_ACCELERATION.get()));
+    /*
+    States: [position, velocity], in meters and meters per second.
+    Inputs (what we can "put in"): [voltage], in volts.
+    Outputs (what we can measure): [position], in meters.
+     */
+    elevatorPlant =
+        LinearSystemId.createElevatorSystem(
+            DCMotor.getNEO(1),
+            ElevatorConstants.CARRIAGE_MASS_KG,
+            ElevatorConstants.DRUM_RADIUS_METERS,
+            ElevatorConstants.ELEVATOR_GEARING);
 
-    feedback.setTolerance(ElevatorConstants.TOLERANCE);
+    observer =
+        new KalmanFilter<>(
+            Nat.N2(),
+            Nat.N1(),
+            (LinearSystem<N2, N1, N1>) elevatorPlant.slice(0),
+            VecBuilder.fill(
+                Units.inchesToMeters(2),
+                Units.inchesToMeters(
+                    40)), // How accurate we think our model is, in meters and meters/second.
+            VecBuilder.fill(
+                0.001), // How accurate we think our encoder position data is. In this case we very
+            // highly trust our encoder position reading.
+            0.02);
+
+    controller =
+        new LinearQuadraticRegulator<>(
+            (LinearSystem<N2, N1, N1>) elevatorPlant.slice(0),
+            VecBuilder.fill(
+                Units.inchesToMeters(1.0), Units.inchesToMeters(10.0)), // qelms. Position
+            // and velocity error tolerances, in meters and meters per second. Decrease this to more
+            // heavily penalize state excursion, or make the controller behave more aggressively. In
+            // this example we weight position much more highly than velocity, but this can be
+            // tuned to balance the two.
+            VecBuilder.fill(
+                12.0), // relms. Control effort (voltage) tolerance. Decrease this to more
+            // heavily penalize control effort, or make the controller less aggressive. 12 is a good
+            // starting point because that is the (approximate) maximum voltage of a battery.
+            0.020); // Nominal time between loops.
+
+    loop =
+        new LinearSystemLoop<>(
+            (LinearSystem<N2, N1, N1>) elevatorPlant.slice(0), controller, observer, 12.0, 0.02);
+
+    // Reset last loop and reference to current state to make sure it's in a known state
+    loop.reset(VecBuilder.fill(inputs.positionMeters, inputs.velocityMetersPerSec));
+    lastProfiledReference =
+        new TrapezoidProfile.State(inputs.positionMeters, inputs.velocityMetersPerSec);
   }
 
   @Override
@@ -48,61 +103,44 @@ public class Elevator extends SubsystemBase {
 
     Logger.processInputs("Elevator", inputs);
 
-    if (DriverStation.isDisabled()) {
-      feedback.reset(inputs.positionMeters, inputs.velocityMetersPerSec);
-    }
-
     if (Constants.tuningMode) {
-      if (ElevatorConstants.KS.hasChanged(hashCode())
-          || ElevatorConstants.KG.hasChanged(hashCode())
-          || ElevatorConstants.KV.hasChanged(hashCode())) {
-        feedforward =
-            new ElevatorFeedforward(
-                ElevatorConstants.KS.get(), ElevatorConstants.KG.get(), ElevatorConstants.KV.get());
-      }
-
-      if (ElevatorConstants.KP.hasChanged(hashCode())
-          || ElevatorConstants.KD.hasChanged(hashCode())
-          || ElevatorConstants.MAX_VELOCITY.hasChanged(hashCode())
+      if (ElevatorConstants.MAX_VELOCITY.hasChanged(hashCode())
           || ElevatorConstants.MAX_ACCELERATION.hasChanged(hashCode())) {
-
-        feedback.setPID(ElevatorConstants.KP.get(), 0, ElevatorConstants.KD.get());
-        feedback.setConstraints(
-            new TrapezoidProfile.Constraints(
-                ElevatorConstants.MAX_VELOCITY.get(), ElevatorConstants.MAX_ACCELERATION.get()));
+        profile =
+            new TrapezoidProfile(
+                new TrapezoidProfile.Constraints(
+                    ElevatorConstants.MAX_VELOCITY.get(),
+                    ElevatorConstants.MAX_ACCELERATION.get()));
       }
     }
 
     if (inputs.limitSwitchPressed) {
       io.resetPosition();
-      if (!isCalibrated) {
-        feedback.reset(ElevatorConstants.STOW);
-        isCalibrated = true;
-      }
     }
 
     Logger.recordOutput("Elevator/PIDEnabled", feedbackMode);
     if (feedbackMode) {
-      io.setVoltage(
-          feedback.calculate(inputs.positionMeters)
-              + feedforward.calculate(
-                  feedback.getSetpoint().position, feedback.getSetpoint().velocity));
+      lastProfiledReference = profile.calculate(0.02, lastProfiledReference, goal);
+      loop.setNextR(lastProfiledReference.position, lastProfiledReference.velocity);
+      loop.correct(VecBuilder.fill(inputs.positionMeters));
+      loop.predict(0.02);
+      io.setVoltage(loop.getU(0));
 
-      Logger.recordOutput("Elevator/Goal/Position", feedback.getGoal().position);
-      Logger.recordOutput("Elevator/Goal/Velocity", feedback.getGoal().velocity);
-      Logger.recordOutput("Elevator/Setpoint/Position", feedback.getSetpoint().position);
-      Logger.recordOutput("Elevator/Setpoint/Velocity", feedback.getSetpoint().velocity);
+      Logger.recordOutput("Elevator/Goal/Position", goal.position);
+      Logger.recordOutput("Elevator/Goal/Velocity", goal.velocity);
+      Logger.recordOutput("Elevator/Setpoint/Position", lastProfiledReference.position);
+      Logger.recordOutput("Elevator/Setpoint/Velocity", lastProfiledReference.velocity);
       Logger.recordOutput("Elevator/Measurement/Position", inputs.positionMeters);
     } else {
-      feedback.reset(
-          new TrapezoidProfile.State(inputs.positionMeters, inputs.velocityMetersPerSec));
+      lastProfiledReference =
+          new TrapezoidProfile.State(inputs.positionMeters, inputs.velocityMetersPerSec);
     }
   }
 
   public Command toSetpoint(double setpointAngle) {
     return Commands.run(
         () -> {
-          feedback.setGoal(setpointAngle);
+          goal = new State(setpointAngle, 0);
           feedbackMode = true;
         },
         this);
@@ -128,9 +166,9 @@ public class Elevator extends SubsystemBase {
                   if (!holdLock) {
                     holdLock = true;
                     if (inputs.velocityMetersPerSec < 0) {
-                      feedback.setGoal(inputs.positionMeters);
+                      goal = new State(inputs.positionMeters, 0);
                     } else {
-                      feedback.setGoal(inputs.positionMeters);
+                      goal = new State(inputs.positionMeters, 0);
                     }
                     feedbackMode = true;
                   }
@@ -144,7 +182,7 @@ public class Elevator extends SubsystemBase {
   }
 
   public boolean atSetpoint() {
-    return feedback.atGoal();
+    return loop.getError(0) < 0.05 && goal.equals(lastProfiledReference);
   }
 
   public boolean limitSwitchPressed() {
