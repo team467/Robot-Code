8c8d3ecfd840c437ccf038368fa80ab86c32ea72
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2025-02-05 20:30:07 -0500

    Added basic comments explaining current orchestrator commands, can be subject to change.

--- src/main/java/frc/robot/Orchestrator.java
+++ src/main/java/frc/robot/Orchestrator.java
@@ -37,10 +37,20 @@ public class Orchestrator {
     this.vision = vision;
   }
 
+  /**
+   * Setting the elevator to duck position.
+   *
+   * @return Command for lowering the elevator.
+   */
   public Command duck() {
     return elevator.toSetpoint(ElevatorConstants.DUCK_POSITION);
   }
 
+  /**
+   * Intakes coral after driving towards the nearest coral station and waiting until intake in position.
+   *
+   * @return Command for intaking coral.
+   */
   public Command intake() {
     return new StraightDriveToPose(getClosestCoralStationPosition(), drive)
         .andThen(
@@ -51,6 +61,13 @@ public class Orchestrator {
         .andThen(coralEffector.intakeCoral());
   }
 
+  /**
+   * Places coral on branch after getting in position.
+   *
+   * @param branchLeft
+   * @param level
+   * @return Command for placing coral.
+   */
   public Command placeCoral(boolean branchLeft, int level) {
 
     return new StraightDriveToPose(getBranchPosition(branchLeft), drive)
@@ -58,6 +75,12 @@ public class Orchestrator {
         .andThen(coralEffector.dumpCoral());
   }
 
+  /**
+   * Removes algae piece after getting in position for algae.
+   *
+   * @param level
+   * @return Command to remove algae from reef.
+   */
   public Command removeAlgae(int level) {
     return new StraightDriveToPose(getBranchPosition(false), drive).andThen(
             elevator.toSetpoint(getAlgaeHeight(
@@ -65,6 +88,12 @@ public class Orchestrator {
         .andThen(algaeEffector.stowArm());
   }
 
+  /**
+   * Gets the level for the branch that we want.
+   *
+   * @param level The level that the coral is on.
+   * @return Command for getting the coral height.
+   */
   public double getCoralHeight(int level) {
     //The default branch we want
     return switch (level) {
@@ -76,6 +105,12 @@ public class Orchestrator {
     }; // subtract offset
   }
 
+  /**
+   * Gets height of algae we want to extract.
+   *
+   * @param level The level that the algae is on.
+   * @return Command for getting the algae height.
+   */
   public double getAlgaeHeight(int level) {
     //double Algae offset
     double offset = 0.0;
@@ -88,6 +123,12 @@ public class Orchestrator {
     return height + offset;
   }
 
+  /**
+   * Gets position of the branch we want.
+   *
+   * @param branchLeft
+   * @return Command for getting branch postion.
+   */
   public Pose2d getBranchPosition(boolean branchLeft) {
     int branch = closestReefFace();
     if (!branchLeft) {
@@ -96,10 +137,16 @@ public class Orchestrator {
     return Reef.branchPositions.get(branch).get(ReefHeight.L1).toPose2d();
   }
 
+  /**
+   * Gets the closest coral station position, either leftCenterFace or rightCenterFace.
+   *
+   * @return Command for getting the closest coral station.
+   */
   public Pose2d getClosestCoralStationPosition() {
 
     return closerToLeftCoralStation() ? CoralStation.leftCenterFace : CoralStation.rightCenterFace;
   }
+
   public boolean closerToLeftCoralStation(){
     double distanceToLeftStation = Math.hypot(Math.abs(drive.getPose().minus(CoralStation.leftCenterFace).getX()), Math.abs(drive.getPose().minus(CoralStation.leftCenterFace).getY()));
     double distanceToRightStation = Math.hypot(Math.abs(drive.getPose().minus(CoralStation.rightCenterFace).getX()), Math.abs(drive.getPose().minus(CoralStation.rightCenterFace).getY()));
