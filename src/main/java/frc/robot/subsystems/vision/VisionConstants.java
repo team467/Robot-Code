403e8c4c10f9a427c8681abcaf5d165af19128f7
theAbishek718 <98927298+theAbishek718@users.noreply.github.com>
2025-02-12 19:34:24 -0500

    Created new branch and added TODO's

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -26,10 +26,12 @@ public class VisionConstants {
   public static Transform3d robotToCamera1 =
       new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
 
+  // TODO: tune these values
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
   public static double maxZError = 0.75;
 
+  // TODO: tune these values
   // Standard deviation baselines, for 1 meter distance and 1 tag
   // (Adjusted automatically based on distance and # of tags)
   public static double linearStdDevBaseline = 0.02; // Meters
