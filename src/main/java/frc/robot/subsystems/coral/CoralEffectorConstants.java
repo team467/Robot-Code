ced7291ff2d6671b2b598defbd83287dddba3560
pgund <183660136+pgund@users.noreply.github.com>
2025-02-20 23:10:08 -0500

    better naming standards for coral

--- src/main/java/frc/robot/subsystems/coral/CoralEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/coral/CoralEffectorConstants.java
@@ -3,14 +3,13 @@ package frc.robot.subsystems.coral;
 import frc.lib.utils.TunableNumber;
 
 public class CoralEffectorConstants {
-  public static TunableNumber CORAL_EFFECTOR_SPEED_OUT =
-      new TunableNumber("CoralEffector/SpeedOut", 0.25);
+  public static TunableNumber CORAL_SPEED_OUT = new TunableNumber("CoralEffector/SpeedOut", 0.25);
 
   public static TunableNumber CORAL_INTAKE_SPEED =
       new TunableNumber("CoralEffector/SpeedIn", 0.175);
   public static TunableNumber CORAL_RETAKE_SPEED =
       new TunableNumber("CoralEffector/RetakeSpeed", -0.15);
-  public static TunableNumber EFFECTOR_HOLD_PERCENT =
+  public static TunableNumber CORAL_HOLD_PERCENT =
       new TunableNumber("CoralEffector/HoldPercent", 0.1);
   public static int effectorCurrentMotorLimit = 35;
   public static int effectorEncoderPositionFactor = 1;
