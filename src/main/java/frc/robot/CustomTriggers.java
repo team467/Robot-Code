c8da8d9e064279e95ea685174a65c783f9b838b7
Elliot Moore <eqm.emoore@hotmail.com>
2025-02-21 00:59:36 -0600

    critical fixes

--- src/main/java/frc/robot/CustomTriggers.java
+++ src/main/java/frc/robot/CustomTriggers.java
@@ -4,29 +4,50 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.Logger;
 
 public class CustomTriggers {
   private static final double JOYSTICK_THRESHOLD = 0.2;
-  private final Map<Trigger, Boolean> lastButtonValues = new HashMap<>();
+  private final Map<Trigger, Boolean> lastTriggerValues = new HashMap<>();
 
   public Trigger toggleOnTrueCancelableWithJoystick(
       Trigger buttonInput, DoubleSupplier X, DoubleSupplier Y) {
+
+    AtomicBoolean toggledState = new AtomicBoolean(false);
+
     return new Trigger(
         CommandScheduler.getInstance().getDefaultButtonLoop(),
         () -> {
           boolean joystickEngaged =
               Math.abs(X.getAsDouble()) > JOYSTICK_THRESHOLD
                   || Math.abs(Y.getAsDouble()) > JOYSTICK_THRESHOLD;
-          boolean lastValue = lastButtonValues.getOrDefault(buttonInput, false);
 
-          if (!joystickEngaged) {
-            boolean currentValue = buttonInput.getAsBoolean();
-            lastButtonValues.put(buttonInput, currentValue);
-            return currentValue != lastValue;
+          boolean lastValue = lastTriggerValues.getOrDefault(buttonInput, false);
+          boolean currentValue = buttonInput.getAsBoolean();
+
+          Logger.recordOutput("CustomJoysticks/Joystick Engaged", joystickEngaged);
+          Logger.recordOutput("CustomJoysticks/Last Value", lastValue);
+          Logger.recordOutput("CustomJoysticks/Current Value", currentValue);
+
+          if (joystickEngaged) {
+            toggledState.set(false);
+            lastTriggerValues.put(buttonInput, false);
+            Logger.recordOutput("CustomJoysticks/Returns", false);
+            return false;
           }
-          lastButtonValues.put(buttonInput, false);
-          return false;
+
+          boolean stateChanged = currentValue && !lastValue;
+          Logger.recordOutput("CustomJoysticks/State Changed", stateChanged);
+
+          if (stateChanged) {
+            toggledState.set(!toggledState.get());
+          }
+
+          lastTriggerValues.put(buttonInput, currentValue);
+          Logger.recordOutput("CustomJoysticks/Returns", toggledState.get());
+          return toggledState.get();
         });
   }
 
@@ -36,6 +57,9 @@ public class CustomTriggers {
       DoubleSupplier Y1,
       DoubleSupplier X2,
       DoubleSupplier Y2) {
+
+    AtomicBoolean toggledState = new AtomicBoolean(false);
+
     return new Trigger(
         CommandScheduler.getInstance().getDefaultButtonLoop(),
         () -> {
@@ -44,15 +68,31 @@ public class CustomTriggers {
                   || Math.abs(Y1.getAsDouble()) > JOYSTICK_THRESHOLD
                   || Math.abs(X2.getAsDouble()) > JOYSTICK_THRESHOLD
                   || Math.abs(Y2.getAsDouble()) > JOYSTICK_THRESHOLD;
-          boolean lastValue = lastButtonValues.getOrDefault(buttonInput, false);
 
-          if (!joystickEngaged) {
-            boolean currentValue = buttonInput.getAsBoolean();
-            lastButtonValues.put(buttonInput, currentValue);
-            return currentValue != lastValue;
+          boolean lastValue = lastTriggerValues.getOrDefault(buttonInput, false);
+          boolean currentValue = buttonInput.getAsBoolean();
+
+          Logger.recordOutput("CustomJoysticks/Joystick Engaged", joystickEngaged);
+          Logger.recordOutput("CustomJoysticks/Last Value", lastValue);
+          Logger.recordOutput("CustomJoysticks/Current Value", currentValue);
+
+          if (joystickEngaged) {
+            toggledState.set(false);
+            lastTriggerValues.put(buttonInput, false);
+            Logger.recordOutput("CustomJoysticks/Returns", false);
+            return false;
           }
-          lastButtonValues.put(buttonInput, false);
-          return false;
+
+          boolean stateChanged = currentValue && !lastValue;
+          Logger.recordOutput("CustomJoysticks/State Changed", stateChanged);
+
+          if (stateChanged) {
+            toggledState.set(!toggledState.get());
+          }
+
+          lastTriggerValues.put(buttonInput, currentValue);
+          Logger.recordOutput("CustomJoysticks/Returns", toggledState.get());
+          return toggledState.get();
         });
   }
 }
