c3f5fa7c00b6cd6c11ccdb5bbd26a8b2b6596a10
pgund <183660136+pgund@users.noreply.github.com>
2025-02-21 18:28:31 -0500

    reduce l4 by a little bit
    Signed-off-by: pgund <183660136+pgund@users.noreply.github.com>

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -155,7 +155,7 @@ public class FieldConstants {
   }
 
   public enum ReefHeight {
-    L4(/*Units.inchesToMeters(72)*/ 0.76, -90),
+    L4(/*Units.inchesToMeters(72)*/ 0.755, -90),
     L3(/*Units.inchesToMeters(47.625)*/ 0.61, -35),
     L2(/*Units.inchesToMeters(31.875)*/ 0.52, -35),
     L1(Units.inchesToMeters(18), 0);
