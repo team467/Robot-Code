08b762a5569f79739c89274ae7cd523cc8c2dbc6
rkris28 <rkris28@students.shrewsbury.k12.ma.us>
2025-03-09 18:35:35 -0400

    Changes First Branch to Right

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -206,7 +206,7 @@ public class RobotContainer {
     autoChooser.addOption("C Score Left", autosAlternate.CScore(true));
     autoChooser.addOption("C Score Right", autosAlternate.CScore(false));
     autoChooser.addOption("B Score Hope and Pray", autosAlternate.BScoreHopeAndPray());
-    autoChooser.addOption("A Sigma Two Score", autosAlternate.sigmaATwoScore(true));
+    autoChooser.addOption("A Sigma Two Score", autosAlternate.sigmaATwoScore(false));
     autoChooser.addOption("A Alpha Three Score", autosAlternate.alphaAThreeScore(true));
     autoChooser.addOption("C Sigma Two Score", autosAlternate.sigmaCTwoScore(true));
     autoChooser.addOption("C Alpha Three Score", autosAlternate.alphaCThreeScore(true));
