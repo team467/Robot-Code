dc041fe9364ccb04ad61f6fb14e48cb16f838dca
shayanHasHackedThis <183660136+pgund@users@>
2025-03-08 19:45:11 -0500

    add autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -204,7 +204,9 @@ public class RobotContainer {
     autoChooser.addOption("B Score Right", autosAlternate.BScore(false));
     autoChooser.addOption("C Score Left", autosAlternate.CScore(true));
     autoChooser.addOption("C Score Right", autosAlternate.CScore(false));
+    autoChooser.addOption("Two Coral", autosAlternate.TwoCoralCScore(false));
     autoChooser.addOption("B Score Hope and Pray", autosAlternate.BScoreHopeAndPray());
+    autoChooser.addOption("Sigma Two Score", autosAlternate.sigmaTwoScore(true));
 
     registerAutoRoutines();
 
