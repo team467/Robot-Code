931c206a96ee6cd29ce309083bdec04f71a01f56
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2025-03-21 19:05:10 -0400

    added TODOs

--- src/main/java/frc/robot/subsystems/fast_algae/FastAlgaeEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/fast_algae/FastAlgaeEffectorConstants.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems.fast_algae;
 
 public class FastAlgaeEffectorConstants {
 
-  public static final double STOW_ANGLE = 0.0;
-  public static final double HIGH_ANGLE = 0.0;
-  public static final double LOW_ANGLE = 0.0;
+  public static final double STOW_ANGLE = 0.0; // TODO: change angle
+  public static final double HIGH_ANGLE = 0.0; // TODO: change angle
+  public static final double LOW_ANGLE = 0.0; // TODO: change angle
 
 
   public static final double HIGH_EXTEND_VOLTAGE = 6;
@@ -13,7 +13,7 @@ public class FastAlgaeEffectorConstants {
   public static final double RETRACT_VOLTAGE = -6;
   public static final double ZERO_VOLTAGE = 0;
 
-  public static boolean PIVOT_INVERTED = true;
+  // public static boolean PIVOT_INVERTED = true;
 
   public static double NEO_PULSES_PER_REVOLUTION = 42;
   public static double PIVOT_GEAR_RATIO = 1;
