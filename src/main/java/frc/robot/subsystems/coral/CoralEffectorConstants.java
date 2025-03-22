012e44dd3ed83289cdf018a9789646afa2c18754
pgund <183660136+pgund@users.noreply.github.com>
2025-03-22 18:33:39 -0400

    bump coral intake speed

--- src/main/java/frc/robot/subsystems/coral/CoralEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/coral/CoralEffectorConstants.java
@@ -8,7 +8,7 @@ public class CoralEffectorConstants {
   public static TunableNumber L1_CORAL_SPEED_OUT =
       new TunableNumber("CoralEffector/SpeedOut", 0.25);
 
-  public static TunableNumber CORAL_INTAKE_SPEED = new TunableNumber("CoralEffector/SpeedIn", 0.2);
+  public static TunableNumber CORAL_INTAKE_SPEED = new TunableNumber("CoralEffector/SpeedIn", 0.3);
   public static TunableNumber CORAL_RETAKE_SPEED =
       new TunableNumber("CoralEffector/RetakeSpeed", -0.15);
   public static TunableNumber CORAL_HOLD_PERCENT =
