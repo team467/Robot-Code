7ffa566e4935b97df081ae4ec7017171cbf69250
rkris28 <rkris28@students.shrewsbury.k12.ma.us>
2025-03-22 22:05:42 -0400

    Format

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOPhysical.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOPhysical.java
@@ -12,7 +12,6 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DigitalInput;
-import frc.robot.FieldConstants.ReefHeight;
 import frc.robot.Schematic;
 
 public class ElevatorIOPhysical implements ElevatorIO {
