6fb746644c5655a9303345ca6141f45c60de151b
ShayanHasNo <147673319+ShayanHasNo@users.noreply.github.com>
2025-03-30 01:02:07 -0400

    whooops

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -241,7 +241,7 @@ public class RobotContainer {
     autoChooser.addOption("Elevator Test", autosAlternate.elevatorRelativeToPose(true, 4));
     autoChooser.addOption("C6-2 Coral", autosAlternate.C6Mpath2Coral());
     autoChooser.addOption("A2-2 Coral", autosAlternate.A2Mpath2Coral());
-    autoChooser.addOption("C6M 3 Coral", runAutonomousAuto("C6M-3Coral"));
+    autoChooser.addOption("C6M 3 Coral", runAutonomousAuto("C6M-3coral"));
     registerAutoRoutines();
 
     // Configure the button bindings
