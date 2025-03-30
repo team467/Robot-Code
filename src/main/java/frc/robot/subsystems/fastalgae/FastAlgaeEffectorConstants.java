2aa39660c913c0b12bd716beaa06afebcc2aaf21
pgund <183660136+pgund@users.noreply.github.com>
2025-03-30 10:00:36 -0400

    remove todos
    Took 1 minute

--- src/main/java/frc/robot/subsystems/fastalgae/FastAlgaeEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/fastalgae/FastAlgaeEffectorConstants.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems.fastalgae;
 
 public class FastAlgaeEffectorConstants {
 
-  public static final double STOW_ANGLE = 0.0; // TODO: change angle
-  public static final double HIGH_ANGLE = 435; // TODO: change angle
-  public static final double LOW_ANGLE = 265; // TODO: change angle
+  public static final double STOW_ANGLE = 0.0;
+  public static final double HIGH_ANGLE = 435;
+  public static final double LOW_ANGLE = 265;
 
   public static final double HIGH_EXTEND_VOLTAGE = 10.0;
   public static final double LOW_EXTEND_VOLTAGE = 10.0;
