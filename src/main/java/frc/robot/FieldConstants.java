d5e14f55af7d08024030a222e07aac3c0915e5c3
Elliot Moore <eqm.emoore@hotmail.com>
2025-05-31 11:02:10 -0500

    New L2 and L3 setpoints for Q5

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -156,8 +156,8 @@ public class FieldConstants {
 
   public enum ReefHeight {
     L4(/*Units.inchesToMeters(72)*/ 0.768, -90),
-    L3(/*Units.inchesToMeters(47.625)*/ 0.633, -35),
-    L2(/*Units.inchesToMeters(31.875)*/ 0.544, -35),
+    L3(/*Units.inchesToMeters(47.625)*/ 0.635, -35),
+    L2(/*Units.inchesToMeters(31.875)*/ 0.546, -35),
     L1(0.467, 0),
     HOME(/*Units.inchesToMeters(17 + 6 / 8)*/ 0.41, 0),
     ALGAE_LOW(/*Units.inchesToMeters(31.875)*/ 0.555, -35),
