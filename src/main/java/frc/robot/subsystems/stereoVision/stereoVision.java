6b03b8b9c0ac54f8a41357b48b0b067e7a190d56
ShayanHásHackedThis <shayansteel117@gmail.com>
2025-11-14 19:52:45 -0500

    have stereovision add the object tranfromation to current pose

--- src/main/java/frc/robot/subsystems/stereoVision/stereoVision.java
+++ src/main/java/frc/robot/subsystems/stereoVision/stereoVision.java
@@ -1,16 +1,33 @@
 package frc.robot.subsystems.stereoVision;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotState;
+import frc.robot.subsystems.drive.Drive;
+import org.littletonrobotics.junction.Logger;
+import java.util.LinkedList;
+import java.util.List;
 
 public class stereoVision extends SubsystemBase {
   private final stereoVisionIO io;
+  private final Drive drive;
   private final stereoVisionInputsAutoLogged inputs;
+  private final List<Pose2d> detectedObjects = new LinkedList<>();
 
-  public stereoVision(stereoVisionIO io) {
+  public stereoVision(stereoVisionIO io, Drive drive) {
     this.io = io;
+    this.drive = drive;
     this.inputs = new stereoVisionInputsAutoLogged();
   }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("StereoVision",inputs);
+    detectedObjects.clear();
+    for (var tranformations: inputs.poseObservations) {
+      detectedObjects.add(drive.getPose().transformBy(tranformations.pose()));
+    }
+
+  }
 }
