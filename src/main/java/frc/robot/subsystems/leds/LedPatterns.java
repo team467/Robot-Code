44f00fe3f5eafd363e9a0d2438bca23dfe5db0d4
EQMoore <117695748+EQMoore@users.noreply.github.com>
2026-01-07 21:13:11 -0500

    clear out game specific LedPatterns

--- src/main/java/frc/robot/subsystems/leds/LedPatterns.java
+++ src/main/java/frc/robot/subsystems/leds/LedPatterns.java
@@ -39,24 +39,6 @@ public enum LedPatterns {
   // Multi-Color Patterns
   RAINBOW(LEDPattern.rainbow(255, 128)),
 
-  // Game Specific
-  STRIPE_ALGAE(
-      LEDPattern.steps(
-          Map.of(
-              0, Color.kGreen, 0.25, Color.kLimeGreen, 0.5, Color.kGreen, 0.75, Color.kLimeGreen))),
-
-  STRIPE_CORAL(
-      LEDPattern.steps(
-          Map.of(
-              0,
-              Color.kPurple,
-              0.25,
-              Color.kMediumPurple,
-              0.5,
-              Color.kPurple,
-              0.75,
-              Color.kMediumPurple))),
-
   GRADIENT_COM(
       LEDPattern.gradient(
           LEDPattern.GradientType.kContinuous, Color.kDarkBlue, Color.kDarkGoldenrod)),
