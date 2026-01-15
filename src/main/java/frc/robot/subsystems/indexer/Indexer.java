2ebcd5ceb91db02b81a917b4b703d2d4335f88fa
EJainDev <ekansh.jain2011@gmail.com>
2026-01-14 20:40:04 -0500

    Remove unused imports

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems.indexer;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase {
