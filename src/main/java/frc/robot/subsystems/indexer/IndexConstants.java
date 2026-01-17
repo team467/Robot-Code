272538755c280bde197d61218fc485ef01c1d2b6
TheKitchenPan1 <arannochak@gmail.com>
2026-01-16 22:16:35 -0500

    changed the name of the intake constant id to indexer motor id

--- src/main/java/frc/robot/subsystems/indexer/IndexConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexConstants.java
@@ -6,7 +6,7 @@ public class IndexConstants {
   public static final double ENCODER_POSITION_CONVERSION;
   public static final double ENCODER_VELOCITY_CONVERSION;
 
-  public static final int INDEXER_INTAKEMOTOR_ID;
+  public static final int INDEXER_INDEXERMOTOR_ID;
   public static final int INDEXER_FEEDUP_ID;
 
   static {
@@ -16,19 +16,19 @@ public class IndexConstants {
         ENCODER_VELOCITY_CONVERSION = 0;
 
         INDEXER_FEEDUP_ID = 0;
-        INDEXER_INTAKEMOTOR_ID = 0;
+        INDEXER_INDEXERMOTOR_ID = 0;
       }
       case ROBOT_BRIEFCASE -> {
         ENCODER_POSITION_CONVERSION = 0;
         ENCODER_VELOCITY_CONVERSION = 0;
         INDEXER_FEEDUP_ID = 0;
-        INDEXER_INTAKEMOTOR_ID = 0;
+        INDEXER_INDEXERMOTOR_ID = 0;
       }
       default -> {
         ENCODER_POSITION_CONVERSION = 0;
         ENCODER_VELOCITY_CONVERSION = 0;
         INDEXER_FEEDUP_ID = 0;
-        INDEXER_INTAKEMOTOR_ID = 0;
+        INDEXER_INDEXERMOTOR_ID = 0;
       }
     }
   }
