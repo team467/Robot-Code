3d5cb1f8a08136fba5f53963bc4a0bde20c94de2
Bhoomi Prashanth <bhoomiprashanth10@gmail.com>
2026-01-17 13:31:25 -0500

    goToSetpoint  = false; changes

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -6,6 +6,7 @@ import org.littletonrobotics.junction.Logger;
 public class Shooter extends SubsystemBase {
   private final ShooterIO io;
   private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+  public boolean setpointEnabled = false;
 
   public Shooter(ShooterIO io) {
     this.io = io;
@@ -14,23 +15,33 @@ public class Shooter extends SubsystemBase {
   @Override
   public void periodic() {
     io.updateInputs(inputs);
-    io.goToSetpoint();
+    
+    if (setpointEnabled) {
+      io.goToSetpoint();
+    }
+    
     Logger.processInputs("Shooter", inputs);
   }
 
   public void setPercent(double percent) {
     io.setPercent(percent);
+    setpointEnabled = false;
   }
 
   public void setVoltage(double volts) {
     io.setVoltage(volts);
+    setpointEnabled = false;
   }
 
   public void stop() {
     io.stop();
+    setpointEnabled = false;
   }
 
   public void setTargetVelocity(double setpoint) {
     io.setTargetVelocity(setpoint);
+    setpointEnabled = true;
   }
+
+  
 }
