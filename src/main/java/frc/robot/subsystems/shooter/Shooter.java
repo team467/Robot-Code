c6e3fc87441c9d5a4f77f0aaee0ba2e5896c6e2a
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2026-01-17 14:39:19 -0500

    changed to commands

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -23,19 +25,28 @@ public class Shooter extends SubsystemBase {
     Logger.processInputs("Shooter", inputs);
   }
 
-  public void setPercent(double percent) {
-    io.setPercent(percent);
-    setpointEnabled = false;
+  public Command stop() {
+    return Commands.runOnce(
+        () -> {
+          io.stop();
+        },
+        this);
   }
 
-  public void setVoltage(double volts) {
-    io.setVoltage(volts);
-    setpointEnabled = false;
+  public Command setPercent(double percent) {
+    return Commands.run(
+        () -> {
+          io.setPercent(percent);
+        },
+        this);
   }
 
-  public void stop() {
-    io.stop();
-    setpointEnabled = false;
+  public Command setVoltage(double volts) {
+    return Commands.run(
+        () -> {
+          io.setVoltage(volts);
+        },
+        this);
   }
 
   public void setTargetVelocity(double setpoint) {
