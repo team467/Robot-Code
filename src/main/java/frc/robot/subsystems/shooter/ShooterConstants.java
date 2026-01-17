c0cddad161d6501114e90d47898515f90f4dc50c
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2026-01-17 15:55:01 -0500

    added follower2 temporary id

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.shooter;
 public class ShooterConstants {
   public static final int LEADER_MOTOR_ID = 0;
   public static final int FOLLOWER_MOTOR_ID = 1;
-public static final double FOLLOWER2_MOTOR_ID = 2;
+  public static final double FOLLOWER2_MOTOR_ID = 2;
 
   public static final double ENCODER_POSITION_CONVERSION = 1.0;
   public static final double ENCODER_VELOCITY_CONVERSION = 1.0;
