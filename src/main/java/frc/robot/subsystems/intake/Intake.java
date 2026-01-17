65ddff6f7c3d998409ee3d1b0bde46b663dc0c5f
TheKitchenPan1 <153659491+TheKitchenPan1@users.noreply.github.com>
2026-01-17 14:49:52 -0500

    added commands also pranav is a bum

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -26,9 +27,15 @@ public class Intake extends SubsystemBase {
   }
 
   public void stop() {
-    io.stop();
+    io.setVoltage(0);
   }
-  public boolean isHopperExtended(){
+
+  public boolean isHopperExtended() {
     return io.isHopperExtended();
   }
+
+  public Command stopCommand(){
+
+  }
+
 }
