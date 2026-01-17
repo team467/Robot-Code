ccd91c8d9abc0e15b8130ca4ae328abccd4d97e2
Niti <ntyag28@students.shrewsbury.k12.ma.us>
2026-01-17 15:17:48 -0500

    arbritrary vals for PID

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -3,11 +3,12 @@ package frc.robot.subsystems.shooter;
 public class ShooterConstants {
   public static final int LEADER_MOTOR_ID = 0;
   public static final int FOLLOWER_MOTOR_ID = 1;
+public static final double FOLLOWER2_MOTOR_ID = 2;
 
   public static final double ENCODER_POSITION_CONVERSION = 1.0;
   public static final double ENCODER_VELOCITY_CONVERSION = 1.0;
 
-  public static final double PID_P = 0.0;
-  public static final double PID_I = 0.0;
-  public static final double PID_D = 0.0;
+  public static final double PID_P = 0.00005; // arbritrary values
+  public static final double PID_I = 0.0000001;
+  public static final double PID_D = 0.0000001;
 }
