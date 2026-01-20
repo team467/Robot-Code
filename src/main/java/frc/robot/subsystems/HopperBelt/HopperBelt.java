19aa23dc8ff8db99383bb7b3b165c6a984076581
Anvi Jain <anvijain51@gmail.com>
2026-01-19 19:01:56 -0500

    Changed package name

--- src/main/java/frc/robot/subsystems/HopperBelt/HopperBelt.java
+++ src/main/java/frc/robot/subsystems/HopperBelt/HopperBelt.java
@@ -3,10 +3,9 @@ package frc.robot.subsystems.hopperbelt;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.hopperbelt.HopperBeltIOInputsAutoLogged;
 
 public class HopperBelt extends SubsystemBase {
-  private final HopperbeltIO io;
+  private final HopperBeltIO io;
   private final HopperBeltIOInputsAutoLogged inputs = new HopperBeltIOInputsAutoLogged();
 
   public HopperBelt(HopperBeltIO io) {
