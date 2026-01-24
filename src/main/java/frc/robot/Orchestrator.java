e13667dbbfdbb34ecfab17001971ec0d153fb033
Anvi Jain <anvijain51@gmail.com>
2026-01-24 14:00:30 -0500

    created command in orchestrator, made a button for that and updated button mapping

--- src/main/java/frc/robot/Orchestrator.java
+++ src/main/java/frc/robot/Orchestrator.java
@@ -1,12 +1,27 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.hopperbelt.HopperBelt;
+import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.shooter.Shooter;
 
 public class Orchestrator {
   private final Drive drive;
+  private final Shooter shooter;
+  private final HopperBelt hopperBelt;
+  private final Indexer indexer;
   private final RobotState robotState = RobotState.getInstance();
 
-  public Orchestrator(Drive drive) {
+  public Orchestrator(Drive drive, HopperBelt hopperBelt, Shooter shooter, Indexer indexer) {
     this.drive = drive;
+    this.hopperBelt = hopperBelt;
+    this.shooter = shooter;
+    this.indexer = indexer;
+  }
+  /** created command to shoot the balls so it runs the shooter, hopperBelt and indexer */
+  public Command shootBalls() {
+    return Commands.parallel(shooter.setVoltage(1), hopperBelt.start(), indexer.run());
   }
 }
