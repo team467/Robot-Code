12fb68a85547463718bbd017b1f96e9925c2384b
Elliot Moore <eqm.emoore@hotmail.com>
2026-01-31 13:27:49 -0600

    Initial drive while shooting logic

--- src/main/java/frc/robot/Orchestrator.java
+++ src/main/java/frc/robot/Orchestrator.java
@@ -1,7 +1,9 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.FieldConstants.Hub;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.hopperbelt.HopperBelt;
 import frc.robot.subsystems.indexer.Indexer;
@@ -27,7 +29,26 @@ public class Orchestrator {
   }
 
   public double getAngleToShoot() {
-    double robotAngle = drive.getPose().getRotation().getDegrees();
-    return 0.0;
+    double robotAngle = drive.getPose().getRotation().getRadians();
+    Translation2d hubPose = Hub.topCenterPoint.toTranslation2d();
+    Translation2d robotPose = drive.getPose().getTranslation();
+    Translation2d robotVelocity = new Translation2d(drive.getChassisSpeeds().vxMetersPerSecond,
+        drive.getChassisSpeeds().vyMetersPerSecond);
+    Translation2d finalTarget = hubPose.minus(projection(hubPose.minus( robotPose), robotVelocity));
+    double angleDifference = Math.acos(finalTarget.dot(robotPose) / (finalTarget.getNorm()*robotPose.getNorm()));
+    return robotAngle + angleDifference;
+  }
+  public double getDistanceToShoot() {
+    Translation2d hubPose = Hub.topCenterPoint.toTranslation2d();
+    Translation2d robotPose = drive.getPose().getTranslation();
+    Translation2d robotVelocity = new Translation2d(drive.getChassisSpeeds().vxMetersPerSecond,
+        drive.getChassisSpeeds().vyMetersPerSecond);
+    Translation2d finalTarget = hubPose.minus(projection(hubPose.minus( robotPose), robotVelocity));
+    return robotPose.minus(finalTarget).getNorm();
+  }
+
+  public Translation2d projection(Translation2d a, Translation2d b){
+    double scalar = (a.dot(b)) / (b.dot(b));
+    return new Translation2d(b.getX() * scalar, b.getY() * scalar);
   }
 }
