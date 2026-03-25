1e119fc6d5a2c065ae7635e7ceccdbf819d88aa0
EJainDev <ekansh.jain2011@gmail.com>
2026-03-25 19:32:03 -0400

    Adjust indexer speed

--- src/main/java/frc/robot/subsystems/indexer/IndexConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexConstants.java
@@ -4,5 +4,5 @@ public class IndexConstants {
   public static final double ENCODER_FEEDUP_POSITION_CONVERSION = 1;
   public static final double ENCODER_FEEDUP_VELOCITY_CONVERSION = 1;
   public static final double PRELOAD_VOLT = 3.0;
-  public static final double FEEDUP_VOLT = 8;
+  public static final double FEEDUP_VOLT = 6;
 }
