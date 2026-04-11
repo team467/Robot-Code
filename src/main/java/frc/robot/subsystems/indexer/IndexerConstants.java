f86c87e116f71fb098851a828e7fcd46f28088c7
Pranav Gundu <pranav@gundu.me>
2026-04-10 22:05:31 -0400

    indexer

---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.indexer;
+
+public class IndexerConstants {
+  public static final double ENCODER_FEEDUP_POSITION_CONVERSION = 1;
+  public static final double ENCODER_FEEDUP_VELOCITY_CONVERSION = 1;
+  public static final double FEEDUP_VOLT = 12;
+}
